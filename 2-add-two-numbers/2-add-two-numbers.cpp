35d52a83bff76ac19af854f4ed9644f6f8ac88ed
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-27 12:41:28 +0000

    Time: 46 ms (41.61%), Space: 71.4 MB (81.05%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -32,25 +32,13 @@ public:
                 l2 = l2->next;
             }
             
-            temp->next = new ListNode((val1+val2+carry)%10);
+            int sum = val1+val2+carry;
+            
+            temp->next = new ListNode(sum%10);
             temp = temp->next;
             
-            carry = (val1+val2+carry)/10;
-            
-            
-            
-//             int val1 = l1? l1->val:0;
-//             int val2 = l2? l2->val:0;
-            
-//             temp->next = new ListNode((val1+val2+carry)%10);
-//             temp = temp->next;
-            
-//             carry = (val1+val2+carry)/10;
-            
-//             if (l1) l1 = l1->next;
-//             if (l2) l2 = l2->next;
-            
-            
+            carry = sum/10;
+                      
         }
         
         return ans->next;
