82d57a07d9e3c10e93321558988602a27fbd8a9d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-27 10:49:50 +0000

    Time: 134 ms (34.27%), Space: 46.5 MB (72.26%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,8 +4,8 @@ public:
         
         sort(nums.begin(), nums.end());
         
-        for(int i = 1; i<nums.size(); i++){
-            if(nums[i-1] == nums[i])
+        for (int i = 0; i<nums.size()-1; i++){
+            if (nums[i]==nums[i+1])
                 return true;
         }
         
