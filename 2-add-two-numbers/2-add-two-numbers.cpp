57c98906365d33c61897535ff2b16028a23f63ea
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-01-27 12:40:37 +0000

    Time: 34 ms (59.92%), Space: 71.5 MB (10.99%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -20,16 +20,35 @@ public:
         
         while(l1 || l2 || carry){
             
-            int val1 = l1? l1->val:0;
-            int val2 = l2? l2->val:0;
+            int val1{}, val2{};
+            
+            if (l1){
+                val1 = l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2){
+                val2 = l2->val;
+                l2 = l2->next;
+            }
             
             temp->next = new ListNode((val1+val2+carry)%10);
             temp = temp->next;
             
             carry = (val1+val2+carry)/10;
             
-            if (l1) l1 = l1->next;
-            if (l2) l2 = l2->next;
+            
+            
+//             int val1 = l1? l1->val:0;
+//             int val2 = l2? l2->val:0;
+            
+//             temp->next = new ListNode((val1+val2+carry)%10);
+//             temp = temp->next;
+            
+//             carry = (val1+val2+carry)/10;
+            
+//             if (l1) l1 = l1->next;
+//             if (l2) l2 = l2->next;
             
             
         }
