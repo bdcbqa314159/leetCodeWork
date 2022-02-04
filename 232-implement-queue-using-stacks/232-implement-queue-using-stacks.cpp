a1e6696679f78a3945009613e7128cacc640f170
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-04 09:20:30 +0000

    Time: 0 ms (100.00%), Space: 7 MB (87.33%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -0,0 +1,88 @@
+class MyQueue {
+public:
+    
+    int nextI, firstI, size, capacity;
+    int* arr;
+    
+    MyQueue():nextI(0), firstI(-1), size(0), capacity(5) {
+        arr = new int[5];
+        
+    }
+    
+    void push(int x) {
+
+		if(size == capacity){
+			int *newArr = new int[2*capacity];
+			int j = 0;
+			for (int i = firstI; i<capacity; i++){
+				newArr[j] = arr[i];
+				j++;
+			}
+			for (int i = 0; i<firstI; i++){
+				newArr[j] = arr[i];
+				j++;
+			}
+
+			firstI = 0;
+			nextI = capacity;
+			capacity *= 2;
+			delete []arr;
+			arr = newArr;
+		}
+
+		arr[nextI] = x;
+		nextI = (nextI+1)%capacity;
+
+		if (firstI == -1){
+			firstI = 0;
+		}
+		size++;
+	}
+    
+    int pop() {
+        
+        if (empty()){
+            cout<<"empty"<<endl;
+            return INT_MIN;
+        }
+        
+        int ans = arr[firstI];
+        arr[firstI] = 0;
+        firstI = (firstI+1)%capacity;
+        
+        size--;
+        
+        if (size==0)
+        {
+            nextI = 0;
+            firstI = -1;
+            
+        }    
+        
+        return ans;
+    }
+    
+    int peek() {
+        
+        if (empty()){
+            cout<<"empty"<<endl;
+            return INT_MIN;
+        }
+        
+        return arr[firstI];
+        
+    }
+    
+    bool empty() {
+        return (size==0);
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
