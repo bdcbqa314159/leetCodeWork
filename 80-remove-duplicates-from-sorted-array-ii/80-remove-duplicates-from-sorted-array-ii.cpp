c6553799040711ab2e2bf038d30c22d089e38a79
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-06 14:49:38 +0000

    Time: 4 ms (86.50%), Space: 10.9 MB (74.56%) - LeetHub

---
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if (n < 3) return n;
+        
+        int i = 1;
+        int j = 2;
+        
+        while(j<n){
+            if (!(nums[j] == nums[i] && nums[j] == nums[i-1])){
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            j++;
+        }
+        
+        return i+1;
+        
+    }
+};
