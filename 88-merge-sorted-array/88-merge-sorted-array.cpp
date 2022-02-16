05be7366ab633d77b9360394982dc8129b84e06e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-16 16:56:22 +0000

    Time: 5 ms (28.41%), Space: 9.1 MB (72.72%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,22 +4,21 @@ public:
         
         int last = m+n-1;
         
-        while( m > 0 && n >0){
-            
-            if (nums1[m-1] > nums2[n-1]){
+        while(m>0 && n>0){
+            if (nums1[m-1]> nums2[n-1]){
                 nums1[last] = nums1[m-1];
+                last--;
                 m--;
             }
             
             else{
                 nums1[last] = nums2[n-1];
+                last--;
                 n--;
-            }
-            
-            last--;
+            } 
         }
         
-        while(n>0){
+        while (n>0){
             nums1[last] = nums2[n-1];
             n--;
             last--;
@@ -27,8 +26,5 @@ public:
         
     }
 };
-
-
-
 
 
