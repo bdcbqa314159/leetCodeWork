cd01d46640e1d51260363a2b0d5a97b866eadd97
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-26 17:47:12 +0000

    Time: 124 ms (71.76%), Space: 67.8 MB (11.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,10 +6,8 @@ public:
         
         for (int i = 1; i<n; i++){
             currMax = max(currMax+nums[i], nums[i]);
-            maxSoFar = max(maxSoFar, currMax);
+            maxSoFar = max(currMax, maxSoFar);
         }
-        
         return maxSoFar;
-        
     }
 };
