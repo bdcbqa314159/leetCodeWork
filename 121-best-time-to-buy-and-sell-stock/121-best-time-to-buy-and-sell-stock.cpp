9c96b5c71d7d4a164b42b0430ab0cc176b439226
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-27 17:00:31 +0000

    Time: 193 ms (45.46%), Space: 93.3 MB (90.36%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, buyPrice = INT_MAX;
+        int profit = 0;
+        int buyPrice = INT_MAX;
+        int n = prices.size();
         
-        for(auto price: prices){
-            if (buyPrice>price)
-                buyPrice = price;
-            else
-                profit = max(profit, price-buyPrice);
+        for (int i = 0; i<n; i++){
+            if(buyPrice>prices[i]){
+                buyPrice = prices[i];
+            }
+            else{
+                profit = max(profit, prices[i]-buyPrice);
+            }
         }
+        
         return profit;
     }
 };
