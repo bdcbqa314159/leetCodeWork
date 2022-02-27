5b356c2bf3465f0eadd8bece2c0b6e2f026a5d53
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-02-27 16:47:32 +0000

    Time: 10 ms (39.24%), Space: 10.7 MB (21.23%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,39 +1,68 @@
 class Solution {
 public:
     
-    unordered_map<int,int> helper(vector<int>& nums){
+    
+    unordered_map<int, int> helper(vector<int>& nums){
+        unordered_map<int, int> result;
         
-        unordered_map<int,int> result;
+        int n = nums.size();
         
-        for (auto n: nums){
-            if (result.find(n) == result.end()){
-                result[n]=1;
+        for(int i = 0; i<n; i++){
+            if (result.find(nums[i]) != result.end()){
+                result[nums[i]]++;
             }
             else{
-                result[n]++;
+                result[nums[i]] = 1;
             }
         }
+        
         return result;
+  
     }
+    
     
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> umap1 = helper(nums1);
         unordered_map<int,int> umap2 = helper(nums2);
         
-        unordered_map<int,int>::iterator it;
+        unordered_map<int, int>::iterator it;
         
-        vector<int> result{};
+        vector<int> answer{};
         
-        
-        for(it = umap1.begin(); it!=umap1.end(); it++){
+        for (it = umap1.begin(); it!= umap1.end(); it++){
+            
             if (umap2.find(it->first) != umap2.end()){
-                int no = min(umap1[it->first], umap2[it->first]);
-                for (int i = 0; i<no; i++){
-                    result.push_back(it->first);
+                int number = min(it->second, umap2[it->first]);
+                for (int i = 0; i<number; i++){
+                    answer.push_back(it->first);
                 }
             }
+            
         }
         
-        return result;
+        return answer;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
