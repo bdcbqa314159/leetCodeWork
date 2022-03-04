a12309719912318aae6030ed3233fa608e002dfb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-04 21:55:44 +0000

    Time: 93 ms (83.25%), Space: 51.3 MB (60.91%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,12 +4,13 @@ public:
         
         unordered_map<int, int> umap;
         
-        for (int& num: nums){
-            if (umap.find(num)!= umap.end()){
+        for (int num: nums){
+            if (umap.find(num)!=umap.end()){
                 return true;
             }
+            
             else{
-                umap[num]=1;
+                umap[num] = 1;
             }
         }
         
