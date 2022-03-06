eedb32b7dca8889acae28b1346c3c571df77a217
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-06 23:20:35 +0000

    Time: 7 ms (66.44%), Space: 10.9 MB (5.57%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     
-    unordered_map<int, int> helper(vector<int>& nums){
+    unordered_map<int,int> helper(vector<int>& nums){
         
-        int n = nums.size();
+        unordered_map<int,int> umap;
         
-        unordered_map<int,int> result;
-        
-        for (int i = 0; i<n; i++){
-            if (result.find(nums[i]) != result.end()){
-                result[nums[i]]++;
+        for( int num: nums){
+            if (umap.find(num)!=umap.end()){
+                umap[num]++;
             }
             else{
-                result[nums[i]] = 1;
+                umap[num] = 1;
             }
         }
         
-        return result;
+        return umap;
     }
     
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result{};
-        unordered_map<int,int> umap1 = helper(nums1);
-        unordered_map<int,int> umap2 = helper(nums2);
+        unordered_map<int, int> umap1 = helper(nums1);
+        unordered_map<int, int> umap2 = helper(nums2);
+        
+        vector<int> result;
         
         unordered_map<int,int>::iterator it;
         
-        for (it = umap1.begin(); it!=umap1.end(); it++){
-            if (umap2.find(it->first) != umap2.end()){
+        for( it = umap1.begin(); it!=umap1.end(); it++){
+            if(umap2.find(it->first)!=umap2.end()){
                 int m = min(it->second, umap2[it->first]);
-                for (int i = 0; i<m; i++)
+                for (int i = 0; i<m; i++){
                     result.push_back(it->first);
+                }
             }
         }
         
         return result;
-        
     }
 };
