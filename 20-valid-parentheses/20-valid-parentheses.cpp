b30a29204e03073524336f42c8e30f90d97fcba8
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-13 13:32:36 +0000

    Time: 3 ms (35.76%), Space: 6.3 MB (80.05%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -35,19 +35,3 @@ public:
     }
 };
 
-// class Solution:
-
-    
-//     def isValid(self, s: str) -> bool:
-//         st = []
-        
-//         for char in s:
-//             if len(st)!=0:
-//                 li = st[-1]
-//                 if self.isEqual(li,char):
-//                     st.pop()
-//                     continue
-//             st.append(char)
-        
-//         return len(st)==0
-        
