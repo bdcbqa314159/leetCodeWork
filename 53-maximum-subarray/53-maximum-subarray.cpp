1e6f3f64fd4f98ee52bb342c11c61e3ba687bc2a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-18 22:36:06 +0000

    Time: 177 ms (30.96%), Space: 67.6 MB (99.60%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int maxCurrent = nums[0], maxSoFar = nums[0];
+        int maxTemp = nums[0], maxFinal = nums[0];
         
         for (int i = 1; i<nums.size(); i++){
-            maxCurrent = max(maxCurrent+nums[i], nums[i]);
-            maxSoFar = max(maxSoFar, maxCurrent);
+            maxTemp = max(nums[i], maxTemp+nums[i]);
+            maxFinal = max(maxTemp, maxFinal);
         }
         
-        return maxSoFar;
         
+        return maxFinal;
         
         
     }
