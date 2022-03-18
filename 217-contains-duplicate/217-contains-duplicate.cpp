a3ee4d68725d12d2ef326b8f195b9e18efd9969f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-18 15:19:48 +0000

    Time: 216 ms (5.89%), Space: 51.5 MB (49.27%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -8,9 +8,10 @@ public:
             if (umap.find(nums[i]) != umap.end())
                 return true;
             else
-                umap[nums[i]] = 1;
+                umap[nums[i]]=1;
         }
-        
         return false;
+        
+        
     }
 };
