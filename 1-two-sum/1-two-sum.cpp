223f403934c00d12bec148278e86a1e6b9746a3c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-19 01:20:34 +0000

    Time: 20 ms (58.67%), Space: 12 MB (11.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -7,9 +7,8 @@ public:
         unordered_map<int,int> umap;
         
         for(int i = 0; i<nums.size(); i++){
-            goal = target - nums[i];
-            
-            if (umap.find(goal) != umap.end()){
+            goal = target-nums[i];
+            if(umap.find(goal) != umap.end()){
                 ans[0] = umap[goal];
                 ans[1] = i;
             }
@@ -17,8 +16,7 @@ public:
             else
                 umap[nums[i]] = i;
         }
+        
         return ans;
-        
-        
     }
 };
