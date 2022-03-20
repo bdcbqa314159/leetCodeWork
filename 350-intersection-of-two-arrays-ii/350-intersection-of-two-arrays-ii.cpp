a7ad11292f3c6a41df4d95feb6694dbaf9b8565c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 14:01:11 +0000

    Time: 11 ms (33.46%), Space: 9.9 MB (99.37%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -8,7 +8,6 @@ public:
         sort(nums2.begin(), nums2.end());
         
         while(i<nums1.size() && j<nums2.size()){
-            
             if (nums1[i]<nums2[j]) i++;
             else if (nums1[i]>nums2[j]) j++;
             else{
@@ -17,7 +16,7 @@ public:
                 j++;
             }
         }
-    
+        
         return answer;
     }
 };
