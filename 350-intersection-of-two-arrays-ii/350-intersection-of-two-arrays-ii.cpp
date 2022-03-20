fabbe9e6f1cac582e97a7c6fa1fffd3eb5139278
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 09:19:50 +0000

    Time: 3 ms (94.72%), Space: 10.1 MB (84.55%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    
-    unordered_map<int,int> helper(vector<int>& nums){
-        
-        unordered_map<int,int> umap;
-        
-        for( int num: nums){
-            if (umap.find(num)!=umap.end()){
-                umap[num]++;
-            }
-            else{
-                umap[num] = 1;
-            }
-        }
-        
-        return umap;
-    }
-    
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> umap1 = helper(nums1);
-        unordered_map<int, int> umap2 = helper(nums2);
+        vector<int> answer;
+        int i=0, j=0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
         
-        vector<int> result;
         
-        unordered_map<int,int>::iterator it;
-        
-        for( it = umap1.begin(); it!=umap1.end(); it++){
-            if(umap2.find(it->first)!=umap2.end()){
-                int m = min(it->second, umap2[it->first]);
-                for (int i = 0; i<m; i++){
-                    result.push_back(it->first);
-                }
+        while(i<nums1.size() && j<nums2.size()){
+            if (nums1[i] < nums2[j])
+                i++;
+            else if(nums1[i]>nums2[j])
+                j++;
+            else{
+                answer.push_back(nums1[i]);
+                i++;
+                j++;
             }
         }
         
-        return result;
+        return answer;
     }
 };
