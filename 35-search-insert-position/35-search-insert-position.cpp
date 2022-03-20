de9065e3a9b2062858e7d6cdbf1e79ff4f148a09
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 15:33:39 +0000

    Time: 8 ms (41.21%), Space: 9.6 MB (73.18%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -5,12 +5,13 @@ public:
         int i = 0, j = nums.size()-1, k = 0;
         
         while(i<=j){
-            k = i +(j-i)/2;
+            k = i + (j-i)/2;
             if (nums[k] == target) return k;
-            else if (nums[k]<target) i = k+1;
+            else if (nums[k] < target) i = k+1;
             else j = k-1;
         }
         
         return i;
+        
     }
 };
