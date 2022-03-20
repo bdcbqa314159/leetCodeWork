0125a83e86e37051c231c7a324e05acca6585646
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 14:56:26 +0000

    Time: 64 ms (26.70%), Space: 27.5 MB (63.86%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -6,12 +6,11 @@ public:
         
         while(i<=j){
             k = i+(j-i)/2;
-            
             if (nums[k] == target) return k;
             else if (nums[k] < target) i = k+1;
             else j = k-1;
         }
-    
+        
         return -1;
     }
 };
