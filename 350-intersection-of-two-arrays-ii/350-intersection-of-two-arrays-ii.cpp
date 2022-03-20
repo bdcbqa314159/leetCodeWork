8fe0b50bb030b293b854304751a0c48ac604e2e1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 09:33:08 +0000

    Time: 0 ms (100.00%), Space: 10.1 MB (84.55%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> answer;
         int i = 0, j = 0;
+        vector<int> answer;
         
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
         
-        while (i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]) i++; 
+        while(i<nums1.size() && j<nums2.size()){
+            if (nums1[i]<nums2[j]) i++;
             else if(nums1[i]>nums2[j]) j++;
+            
             else{
                 answer.push_back(nums1[i]);
                 i++;
@@ -19,6 +20,5 @@ public:
         }
         
         return answer;
-    
     }
 };
