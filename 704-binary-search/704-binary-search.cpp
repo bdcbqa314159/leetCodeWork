a40d92dcd5fadc7c3c693a890a7fb36b8b80b6c6
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 14:55:12 +0000

    Time: 55 ms (44.30%), Space: 27.7 MB (13.55%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        int left = 0, right = nums.size()-1, medium = 0;
+        int i = 0, j = nums.size()-1, k = 0;
         
-        while(left<=right){
-            medium = left+(right-left)/2;
+        while(i<=j){
+            k = i+(j-i)/2;
             
-            if (nums[medium] == target){
-                return medium;
-            }
-            
-            else if (nums[medium]<target) left = medium+1;
-            else right = medium-1;
+            if (nums[k] == target) return k;
+            else if (nums[k] < target) i = k+1;
+            else j = k-1;
         }
-        
+    
         return -1;
-        
     }
 };
