d30d5ee468c3f19f3c6db9c43e40c1e20e21d84d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 15:13:29 +0000

    Time: 2 ms (34.05%), Space: 5.8 MB (89.43%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,15 +4,12 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        
         int i = 1, j = n, k = 0;
-        
         while(i<=j){
             k = i+(j-i)/2;
             if (isBadVersion(k)) j = k-1;
             else i = k+1;
         }
-        
         return i;
     }
 };
