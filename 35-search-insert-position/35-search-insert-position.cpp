aae92de6c692c7afd069a2af5ad5975dd4e6712a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-20 15:18:39 +0000

    Time: 16 ms (6.54%), Space: 9.6 MB (73.18%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
         int i = 0, j = nums.size()-1, k = 0;
         
         while(i<=j){
             k = i+(j-i)/2;
-            
             if (nums[k] == target) return k;
             else if (nums[k]<target) i = k+1;
             else j = k-1;
-            
         }
         
-        return i;
         
+        return i;
     }
 };
