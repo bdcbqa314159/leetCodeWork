43bb831e4da8241f80261ee7d73f0a1a69e8889c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-22 08:38:10 +0000

    Time: 31 ms (56.77%), Space: 21.8 MB (7.41%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -7,16 +7,16 @@ public:
         
         for (int i = 0; i<r; i++){
             for (int j = 0; j<c; j++){
-                
                 char ch = board[i][j];
-                if(ch != '.' && (rows[i][ch]>0 || columns[j][ch] || boxes[i/3*3+j/3][ch]>0)) 
-                    return false;
                 
+                if (ch != '.' && (rows[i][ch]>0 || columns[j][ch]>0 || boxes[i/3*3+j/3][ch]>0))
+                    return false;
                 rows[i][ch]++;
                 columns[j][ch]++;
                 boxes[i/3*3+j/3][ch]++;
             }
         }
+        
         
         return true;
     
