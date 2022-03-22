de728568bf33bc6ab1e9a809f01e9eb131605754
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-22 08:01:20 +0000

    Time: 0 ms (100.00%), Space: 6.6 MB (33.57%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
+        vector<vector<int>> r(numRows);
         
-        if (numRows == 1){
-            vector<int> a(1,1);
-            result.push_back(a);
-            return result;
-        }
-        
-        else if(numRows == 2){
-            vector<int> a(1,1);
-            result.push_back(a);
-            a.resize(2,1);
-            result.push_back(a);
-            return result;
-        }
-        
-        else{
-            vector<int> a(1,1);
-            result.push_back(a);
-            a.resize(2,1);
-            result.push_back(a);
+        for (int i = 0; i<numRows; i++){
+            r[i].resize(i+1);
+            r[i][0] = r[i][i] = 1;
             
-            for (int i = 3; i<=numRows; i++){
-                vector<int> temp{};
-                temp.push_back(1);
-                for(int j = 1; j<a.size(); j++){
-                    temp.push_back(a[j]+a[j-1]);
-                }
-                
-                temp.push_back(1);
-                a.resize(temp.size());
-                a=temp;
-                result.push_back(a);
+            for (int j = 1; j<i; j++){
+                r[i][j] = r[i-1][j-1]+r[i-1][j];
             }
-            
-            return result;
         }
+        return r;
     }
 };
-
