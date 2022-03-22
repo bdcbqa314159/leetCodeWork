86cfe7f1afa0ec22118667562b13b1a870d0a4f5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-22 09:36:51 +0000

    Time: 4 ms (78.14%), Space: 9.6 MB (52.47%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int r = matrix.size(), c = matrix[0].size(), a = 0 , b = r-1, row = 0, mid = 0;
+        int r = matrix.size(), c = matrix[0].size(), a = 0, b = r-1, row = 0, mid = 0;
         
         while(a<=b){
-            row = a + (b-a)/2;
-            
-            if (target> matrix[row][c-1]) a = row+1;
-            else if (target<matrix[row][0]) b = row-1;
+            row = a+(b-a)/2;
+            if (target > matrix[row][c-1]) a = row+1;
+            else if (target < matrix[row][0]) b = row-1;
             else break;
         }
         
         if (a>b) return false;
-        row = a + (b-a)/2, a = 0, b = c-1;
+        
+        row = a+ (b-a)/2; a = 0; b = c-1;
         
         while(a<=b){
             mid = a + (b-a)/2;
-            
-            if (target<matrix[row][mid]) b = mid-1;
-            else if (target>matrix[row][mid]) a = mid+1;
+            if (target > matrix[row][mid]) a = mid +1;
+            else if (target < matrix[row][mid]) b = mid-1;
             else return true;
         }
-        
-        
         
         return false;
     }
