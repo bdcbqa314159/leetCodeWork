3b4e60da14f9bc5ca46cac62773318f2bcaf62a5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-27 19:00:00 +0100

    Time: 0 ms (100.00%), Space: 6.3 MB (79.70%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -6,22 +6,27 @@ public:
         if (c1 == '(' && c2 == ')') return true;
         if (c1 == '[' && c2 == ']') return true;
         if (c1 == '{' && c2 == '}') return true;
+        
+        
         return false;
     }
+    
     
     bool isValid(string s) {
         vector<char> st{};
         
-        for (char &c: s){
-            if (st.size() != 0){
+        for (char &c : s){
+            if (st.size()!=0){
                 char li = st[st.size()-1];
-                if (isEqual(li, c)){
+                if (isEqual(li,c)){
                     st.pop_back();
                     continue;
                 }
             }
+            
             st.push_back(c);
         }
+        
         
         return (st.size() == 0);
     }
