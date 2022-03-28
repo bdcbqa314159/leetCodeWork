bbaaa456045736bc7e0ce71eff928f9f5e828cf2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-29 00:20:55 +0100

    Time: 4 ms (21.17%), Space: 7 MB (54.53%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,42 +1,29 @@
 class MyQueue {
+
+   
+    
 public:
+    
     int nextI, firstI, size, capacity;
-    int* arr;
+    int *arr;
     
     MyQueue(): nextI(0), firstI(-1), size(0), capacity(5){
         arr = new int[capacity];
     }
     
-    void push(int x){
-        if (size == capacity){
-            int *newArr = new int[2*capacity];
-            int j = 0;
-            for (int i = firstI; i<capacity; i++){
-                newArr[i] = arr[i];
-                j++;
-            }
-            for (int i = 0;i <firstI; i++){
-                newArr[i]=arr[i];
-                j++;
-            }
-            
-            firstI = 0;
-            nextI = capacity;
-            capacity*=2;
-            delete []arr;
-            arr = newArr;
-        }
-        arr[nextI] = x;
-        nextI = (nextI+1)%capacity;
-        
-        if (firstI == -1){
-            firstI = 0;
+    bool empty(){
+        return (size==0);
+    }    
+    
+    int peek(){
+        if (empty()){
+            cout<<"empty"<<endl;
+            return INT_MIN;
         }
         
-        size++;
-        
+        return arr[firstI];
     }
-   
+    
     int pop(){
         if (empty()){
             cout<<"empty"<<endl;
@@ -48,26 +35,45 @@ public:
         firstI = (firstI+1)%capacity;
         size--;
         
-        if (size == 0){
-            nextI = 0;
+        if(size == 0){
             firstI = -1;
+            nextI = 0;
         }
-        
         return ans;
     }
     
-    int peek(){
-        if (empty()){
-            cout<<"empty"<<endl;
-            return INT_MIN;
+    void push(int x){
+        if (size==capacity){
+            int *newArr = new int[2*capacity];
+            int j = 0;
+            
+            for (int i = firstI; i<capacity; i++){
+                newArr[i] = arr[i];
+                j++;
+            }
+            
+            for (int i = 0; i<firstI; i++){
+                newArr[i] = arr[i];
+                j++;
+            }
+            firstI = 0;
+            
+            nextI = capacity;
+            capacity*=2;
+            delete []arr;
+            arr = newArr;
         }
-        return arr[firstI];
+        
+        arr[nextI] = x;
+        nextI = (nextI+1)%capacity;
+        if (firstI==-1){
+            firstI = 0;
+        }
+        
+        size++;
     }
     
-    bool empty(){
-        return (size==0);
-    }
-    
+
 };
 
 /**
