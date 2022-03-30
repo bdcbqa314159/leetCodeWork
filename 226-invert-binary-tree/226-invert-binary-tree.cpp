9f95ab9125c95a555ec6b435c336de79ec9f25c2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-30 21:45:30 +0100

    Time: 0 ms (100.00%), Space: 9.8 MB (38.49%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -12,7 +12,6 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
         if (!root) return root;
         TreeNode* left = invertTree(root->left);
         TreeNode* right = invertTree(root->right);
@@ -21,7 +20,5 @@ public:
         root->right = left;
         
         return root;
-        
     }
 };
-
