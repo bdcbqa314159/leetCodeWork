e5cd3e1982aa66a3b418d162fac265fc1b4ec84d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-30 23:14:14 +0100

    Time: 163 ms (15.45%), Space: 57 MB (49.37%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -21,6 +21,7 @@ public:
             root->left = insertIntoBST(root->left, val);
         else if (val > root->val)
             root->right = insertIntoBST(root->right, val);
+        
         return root;
     }
 };
