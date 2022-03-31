a6b2cf7b30c999ef25caa650dcb8baca5dc0c427
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-03-31 14:52:32 +0100

    Time: 87 ms (13.08%), Space: 36.8 MB (63.95%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -13,11 +13,15 @@ class Solution {
 public:
     
     void helper(TreeNode* root, vector<int>& ans){
+        
         if (!root) return;
+        
         helper(root->left, ans);
         ans.push_back(root->val);
         helper(root->right, ans);
+        
     }
+    
     
     vector<int> inOrderTraversal(TreeNode* root){
         vector<int> ans;
@@ -25,15 +29,17 @@ public:
         return ans;
     }
     
+    
+    
     bool findTarget(TreeNode* root, int k) {
         vector<int> tree = inOrderTraversal(root);
         int l = 0, r = tree.size()-1;
+        
         while(l<r){
             int sum = tree[l]+tree[r];
             if (sum == k) return true;
-            else if (sum > k) r--;
+            else if(sum > k) r--;
             else l++;
-        }
-        return false;
-    } 
+        }return false;
+    }
 };
