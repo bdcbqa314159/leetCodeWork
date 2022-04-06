0a20041e0bf4af402183010d8506fe15445913e2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-06 23:16:36 +0100

    Time: 3 ms (29.72%), Space: 6.6 MB (33.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -8,8 +8,6 @@ public:
             r[i][0] = r[i][i] = 1;
             for (int j = 1; j<i; j++)
                 r[i][j] = r[i-1][j-1]+r[i-1][j];
-        }
-        
-        return r;
+        }return r;
     }
 };
