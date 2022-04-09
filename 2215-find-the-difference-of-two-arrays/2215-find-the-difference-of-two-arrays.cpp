d57981dd3e3f15fd8e01d82a6fd6888fcb2435fa
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-09 11:50:26 +0100

    Time: 58 ms (84.75%), Space: 32.6 MB (61.40%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -10,24 +10,22 @@ public:
         return umap;
     }
     
-    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> u = helper(nums1), v = helper(nums2);
-        vector<vector<int>> out;
+    vector<int> helper(unordered_map<int,int>& u, unordered_map<int,int>& v){
         vector<int> a{};
         unordered_map<int,int>::iterator it = u.begin();
         for (it; it!=u.end(); it++){
             if (v.find(it->first)==v.end())
                 a.push_back(it->first);
         }
+        return a;
+    }
+    
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> u = helper(nums1), v = helper(nums2);
+        vector<vector<int>> out;
+        vector<int> a  = helper(u,v), b = helper(v,u);
         out.push_back(a);
-        a.clear();
-        it = v.begin();
-        for (it; it!=v.end(); it++){
-            if (u.find(it->first)==u.end())
-                a.push_back(it->first);
-        }
-        out.push_back(a);
-        a.clear();
+        out.push_back(b);
         return out;
     }
 };
