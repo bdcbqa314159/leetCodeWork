0a345c3c0e2166feeaa1932dfca49a382bd6c0e1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-09 16:45:48 +0100

    Time: 0 ms (100.00%), Space: 6.2 MB (53.15%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string toLowerCase(string s) {
         char ch;
-        // string u = "";
         for (int i = 0; i<s.size(); i++){
             ch = tolower(s[i]);
             s[i] = ch;
