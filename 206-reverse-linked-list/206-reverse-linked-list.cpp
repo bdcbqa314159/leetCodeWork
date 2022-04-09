887fadd4a9144ab86a4de5dcb39c11e917993a25
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-09 12:51:59 +0100

    Time: 18 ms (5.06%), Space: 8.3 MB (80.01%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,8 +11,8 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *prev = NULL, *next = NULL;
-        while(head){
+        ListNode *prev = NULL,*next = NULL;
+        while (head){
             next = head->next;
             head->next = prev;
             prev = head;
