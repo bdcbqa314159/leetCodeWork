c14edfb9af155eb414c5f8223b57db8bde269ac2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-09 10:40:58 +0100

    Time: 30 ms (56.47%), Space: 15.4 MB (9.93%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -13,10 +13,8 @@ public:
     ListNode* removeElements(ListNode* head, int val) {
         if (!head) return head;
         ListNode *h = removeElements(head->next, val);
-        
         if (head->val == val) return h;
-        head->next = h;
+        head->next= h;
         return head;
-        
     }
 };
