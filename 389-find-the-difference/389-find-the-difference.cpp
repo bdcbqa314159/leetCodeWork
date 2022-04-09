4b4c6241a9e6d34f12517a11bfd0f3dbf70284c6
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-09 16:38:31 +0100

    Time: 3 ms (77.92%), Space: 6.7 MB (24.16%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    
+    vector<int> helper(string &s){
+        vector<int> u(26,0);
+        for (int i = 0; i<s.size(); i++)
+            u[s[i]-'a']++;
+        return u;
+    }
+    
     char findTheDifference(string s, string t) {
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        int i = 0;
-        while( s[i] == t[i] ){
-            i++;
+        char ans;
+        vector<int> u = helper(s), v = helper(t);
+        for (int i = 0; i<26; i++){
+            if (v[i]-u[i] == 1){
+                ans = i+'a';
+                break;
+            }
         }
-        
-        return t[i];
-        
+        return ans;
     }
 };
