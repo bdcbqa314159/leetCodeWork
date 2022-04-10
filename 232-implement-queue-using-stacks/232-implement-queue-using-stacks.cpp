ea9752f14f4a8e5da02e9835392a573cfdfba61a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-10 21:51:45 +0100

    Time: 6 ms (12.93%), Space: 7 MB (89.19%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -3,7 +3,7 @@ public:
     
     int firstI, nextI, size, capacity, *arr;
     
-    MyQueue(): firstI(-1), nextI(0), size(0), capacity(3) {
+    MyQueue():firstI(-1), nextI(0), size(0), capacity(3) {
         arr = new int[capacity];
     }
     
