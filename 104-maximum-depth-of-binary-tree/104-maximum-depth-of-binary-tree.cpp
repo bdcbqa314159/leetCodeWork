9a116de82a96fdc789666e45b95697b00aa17f11
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-12 02:55:51 +0100

    Time: 12 ms (53.90%), Space: 18.7 MB (99.33%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root) return 0;
+        if (!root) return {};
         return max(maxDepth(root->left), maxDepth(root->right))+1;
     }
 };
