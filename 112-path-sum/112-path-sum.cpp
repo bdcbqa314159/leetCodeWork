a23b084f43b3ee2d7a846e85eba1c200f2a85995
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-14 18:23:54 +0100

    Time: 12 ms (72.87%), Space: 21.4 MB (40.34%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -16,9 +16,8 @@ public:
         
         if (!root->left && !root->right){
             if (root->val == targetSum) return true;
-            else return false;
+            return false;
         }
-        
-        return hasPathSum(root->left, targetSum-root->val)||hasPathSum(root->right, targetSum-root->val);
+        return hasPathSum(root->right, targetSum-root->val)||hasPathSum(root->left, targetSum-root->val);
     }
 };
