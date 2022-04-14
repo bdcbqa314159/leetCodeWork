29099280b4ef598503f8d99d048d178c1858ac5f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-14 18:30:30 +0100

    Time: 8 ms (91.60%), Space: 21.3 MB (40.34%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root) return false;
-        if (!root->right && !root->left)
+        if (!root->left && !root->right) 
             return root->val == targetSum;
         return hasPathSum(root->left, targetSum-root->val)||hasPathSum(root->right, targetSum-root->val);
     }
