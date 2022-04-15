c31fe941687be8c7791b71f84639c72a99f42e13
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-15 14:51:06 +0100

    Time: 15 ms (65.03%), Space: 21.5 MB (89.05%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -13,13 +13,12 @@ class Solution {
 public:
     
     bool helper(TreeNode *root, long long m = -10000000000, long long M = 10000000000){
-        if (!root) return true; bool l,r;
+        if (!root) return true;
+        bool l,r;
         l = helper(root->left, m, root->val);
         r = helper(root->right, root->val, M);
-        
         return l && r && m<root->val && root->val<M;
-                                                                                    }
-    
+    }
     
     bool isValidBST(TreeNode* root) {
         return helper(root);
