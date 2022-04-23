6ca3ee379e1d7c75abf34f4efddfe0925234436b
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-04-23 11:17:05 +0100

    Time: 128 ms (43.97%), Space: 53.4 MB (21.63%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     
-    unordered_map<int, int> helper(vector<int>& nums){
-        unordered_map<int, int> umap{};
+    unordered_map<int,int> helper(vector<int>& nums){
+        unordered_map<int,int> out;
         for (int i = 0; i<nums.size(); i++){
-            if (umap.find(nums[i]) == umap.end())
-                umap[nums[i]] = 1;
-            else umap[nums[i]]++;
+            auto it = out.find(nums[i]);
+            if (it == out.end()) out[nums[i]] = 1;
+            else out[nums[i]]++;
         }
-        return umap;
+        return out;
     }
     
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> u = helper(nums);
+        unordered_map<int,int> umap = helper(nums);
         for (int i = 0; i<nums.size(); i++){
-            if (u[nums[i]] >1) return true;
+            if (umap[nums[i]]>1) return true;
         }
         return false;
     }
