e70b9049bd40e1dff29e76c8772d19a028fac117
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-06 22:25:35 +0100

    Time: 39 ms (77.68%), Space: 19.6 MB (80.25%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,18 +3,18 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> results;
         
-        if (strs.size() == 0) return results;
+        unordered_map<string, vector<string>> umap;
         
-        unordered_map<string, vector<string>> map;
-        
-        for (string &s: strs){
+        for (string &s : strs){
             string key = s;
-            std::sort(key.begin(), key.end());
-            map[key].push_back(s);
+            sort(key.begin(), key.end());
+            umap[key].push_back(s);
         }
         
-        for (auto &p: map)
+        for (auto &p: umap){
             results.push_back(p.second);
+        }
+        
         
         return results;
     }
