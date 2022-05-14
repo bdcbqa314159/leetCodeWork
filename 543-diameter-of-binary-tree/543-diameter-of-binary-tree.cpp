b96d6cc4d3dcfb4e9d36a03e9535486912c76abb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-14 16:03:38 +0100

    Time: 12 ms (67.63%), Space: 20.6 MB (12.26%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -12,17 +12,16 @@
 class Solution {
 public:
     
-    int helper(TreeNode* node, int &h){
-        if (!node){
+    int helper(TreeNode* node, int& h){
+        if(!node){
             h = 0;
             return 0;
         }
         int h1 = 0, h2 = 0;
         int d1 = helper(node->left, h1), d2 = helper(node->right, h2);
-        h = max(h1, h2)+1;
+        h = max(h1,h2)+1;
         return max(max(d1, d2), h1+h2);
     }
-    
     
     int diameterOfBinaryTree(TreeNode* root) {
         int h = 0;
