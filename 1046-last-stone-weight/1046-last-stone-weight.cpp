075a2825bed29e6b76c74bdf9cc2ce98c3e1236c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-14 16:22:54 +0100

    Time: 8 ms (6.37%), Space: 7.5 MB (78.21%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
+        priority_queue<int>pq;
+        
         for (auto &stone: stones)
             pq.push(stone);
         
         while(pq.size()>1){
-            int x = pq.top();pq.pop();
-            int y = pq.top();pq.pop();
-            if (x>y)
-                pq.push(x-y);
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if (x>y) pq.push(x-y);
         }
         
-        return pq.empty()?0:pq.top();
+        return pq.empty()? 0: pq.top();
     }
 };
