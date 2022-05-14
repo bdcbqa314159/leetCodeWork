3e30d475b643ebcef82808d4d11ba52ed4e530df
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-14 16:58:46 +0100

    Time: 154 ms (71.42%), Space: 83.9 MB (53.70%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -4,18 +4,19 @@ public:
         if (nums.size() == 0) return 0;
         
         unordered_map<int,int> umap;
+        
         int r = 0, s = 0;
         
         for (int i = 0; i<nums.size(); i++){
-            s += nums[i] == 0 ? -1:1;
+            s += nums[i] == 0?-1:1;
             
             if (s == 0){
-                if (r<i+1) r = i+1;
+                if (r<i+1) r=i+1;
             }
-            
             else if (umap.find(s) != umap.end()){
                 if (r<i-umap[s]) r = i-umap[s];
             }
+            
             else
                 umap[s] = i;
         }
