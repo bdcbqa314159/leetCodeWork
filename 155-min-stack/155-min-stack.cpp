2c12807e5b06e4e674e190610bec4ab3e7d4025f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-14 15:32:53 +0100

    Time: 27 ms (64.98%), Space: 16.5 MB (32.73%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,7 +1,6 @@
 class MinStack {
 public:
     
-    
     stack<int> s1, s2;
     
     MinStack() {
@@ -10,7 +9,7 @@ public:
     
     void push(int val) {
         s1.push(val);
-        s2.push((!s2.empty() && s2.top() < val) ? s2.top() : val);
+        s2.push( (!s2.empty() && s2.top()<val) ? s2.top(): val);
     }
     
     void pop() {
