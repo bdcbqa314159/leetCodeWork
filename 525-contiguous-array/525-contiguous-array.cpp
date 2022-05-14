783b899cb47eec08b00bb3680349120269d60e26
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-14 16:51:57 +0100

    Time: 201 ms (32.70%), Space: 83.9 MB (27.63%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,13 +3,14 @@ public:
     int findMaxLength(vector<int>& nums) {
         if (nums.size() == 0) return 0;
         unordered_map<int,int> umap;
-        int s = 0, r = 0;
-        for (int i = 0; i<nums.size(); i++){
-            s += nums[i] == 0 ? -1:1;
+        int r = 0, s = 0;
+        
+        for (int i = 0; i< nums.size(); i++){
+            s += nums[i] == 0?-1:1;
             if (s == 0){
                 if (r<i+1) r = i+1;
             }
-            else if(umap.find(s) != umap.end()){
+            else if (umap.find(s)!=umap.end()){
                 if (r<i-umap[s]) r = i-umap[s];
             }
             
