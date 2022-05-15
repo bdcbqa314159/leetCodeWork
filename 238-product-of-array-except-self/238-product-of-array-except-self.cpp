875d3977d85b159abbd2fc5c8120275aa2cb42c4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-15 02:00:58 +0100

    Time: 44 ms (19.55%), Space: 24 MB (57.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n,1);
+        vector<int> res(n,1);
         
         for (int i = 1; i<n; i++)
-            result[i] = result[i-1]*nums[i-1];
+            res[i] = res[i-1]*nums[i-1];
         
         int rightProd = nums[n-1];
         
         for (int i = n-2; i>=0; i--){
-            result[i] *= rightProd;
+            res[i] *= rightProd;
             rightProd *= nums[i];
         }
         
-        return result;
+        return res;
+        
     }
 };
