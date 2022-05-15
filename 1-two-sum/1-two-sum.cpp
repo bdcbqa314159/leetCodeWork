da0dc0d08ece187528eeb6bcfa9a19de799e27cd
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-15 14:05:05 +0100

    Time: 12 ms (83.45%), Space: 10.9 MB (45.62%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> umap;
+        unordered_map<int, int> umap;
         int i, goal;
         for (i = 0; i<nums.size(); i++){
             goal = target-nums[i];
-            if (umap.find(goal) != umap.end())
+            if (umap.find(goal)!=umap.end())
                 break;
             else
                 umap[nums[i]] = i;
