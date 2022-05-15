6130bfcce51fd496ccba66dde3f7285a29a4f335
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-15 01:58:55 +0100

    Time: 27 ms (77.04%), Space: 24 MB (81.60%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -8,9 +8,10 @@ public:
             result[i] = result[i-1]*nums[i-1];
         
         int rightProd = nums[n-1];
-        for (int i = n-2; i>= 0; i--){
-            result[i]*=rightProd;
-            rightProd*=nums[i];
+        
+        for (int i = n-2; i>=0; i--){
+            result[i] *= rightProd;
+            rightProd *= nums[i];
         }
         
         return result;
