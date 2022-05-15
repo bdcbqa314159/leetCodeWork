c76fbb53f69f1ed1dd43f00caa59ab52b59b779e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-15 13:02:20 +0100

    Time: 0 ms (100.00%), Space: 6.2 MB (59.20%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     bool checkValidString(string s) {
         int n = s.length(), balance = 0;
+        
         for (int i = 0; i<n; i++){
             if (s[i] == ')') balance--;
             else balance++;
             if (balance<0) return false;
         }
-        
         if (balance == 0) return true;
+        
         balance = 0;
+        
         for (int i = n-1; i>=0; i--){
             if (s[i] == '(') balance--;
             else balance++;
