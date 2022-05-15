9220b0cfd39223b68579be30f34ffb3083196b4f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-15 12:49:32 +0100

    Time: 4 ms (30.36%), Space: 6 MB (95.55%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -7,22 +7,21 @@ public:
         int balance = 0;
         
         for (int i = 0; i<n; i++){
-            if (s[i] == ')')
+            if (s[i]==')')
                 balance--;
             else
                 balance++;
             if (balance<0) return false;
         }
-        if (balance == 0) return true;
         
         balance = 0;
         
         for (int i = n-1; i>=0; i--){
             if (s[i] == '(')
                 balance--;
-            else
-                balance++;
-            if (balance<0) return false;
+            else balance++;
+            
+            if (balance <0) return false;
         }
         
         return true;
