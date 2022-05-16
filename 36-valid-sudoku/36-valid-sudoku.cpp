dd61d283ffe17a7d06140c5def6b59fc6bbc4aef
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-16 15:35:11 +0100

    Time: 78 ms (5.07%), Space: 21.9 MB (7.81%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,12 +3,11 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int r = board.size(), c = board[0].size();
         unordered_map<char, int> rows[9], cols[9], boxes[9];
-        
         for (int i = 0; i<r; i++){
-            for (int j = 0; j<c ; j++){
+            for (int j = 0; j<c; j++){
                 char c = board[i][j];
                 if (c!='.' && (rows[i][c]>0 || cols[j][c]>0 || boxes[i/3*3+j/3][c]>0)) return false;
-                rows[i][c]++;cols[j][c]++;boxes[i/3*3+j/3][c]++;
+                rows[i][c]++; cols[j][c]++; boxes[i/3*3+j/3][c]++;
             }
         }return true;
     }
