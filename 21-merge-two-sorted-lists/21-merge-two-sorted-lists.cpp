1adf77fb4736547f73de1ff186ac49b9592915d4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-16 11:36:52 +0100

    Time: 18 ms (12.82%), Space: 14.7 MB (82.53%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -13,17 +13,16 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (!list1) return list2;
         if (!list2) return list1;
-        
         ListNode* out;
         
         if (list1->val<list2->val){
             out = list1;
             out->next = mergeTwoLists(list1->next, list2);
         }
+        
         else{
             out = list2;
             out->next = mergeTwoLists(list2->next, list1);
-        }
-        return out;
+        }return out;
     }
 };
