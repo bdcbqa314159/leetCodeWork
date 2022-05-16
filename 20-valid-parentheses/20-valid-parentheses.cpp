05cca4c895c028dbaad12cf650dec55f40c3c24a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-16 09:01:11 +0100

    Time: 0 ms (100.00%), Space: 6.4 MB (10.63%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     
     bool helper(char a, char b){
-        return (a == '(' && b == ')' || a == '{' && b == '}' || a == '[' && b == ']');
+        return (a=='(' && b==')' || a=='{' && b=='}' || a=='[' && b==']');
     }
     
     bool isValid(string s) {
-        vector<char> st{};
+        stack<char> t;
         for (int i = 0; i<s.size(); i++){
-            if (st.size() != 0){
-                char li = st[st.size()-1];
+            if (!t.empty()){
+                char li = t.top();
                 if (helper(li, s[i])){
-                    st.pop_back();
+                    t.pop();
                     continue;
                 }
             }
-            st.push_back(s[i]);
+            t.push(s[i]);
         }
-        return st.size() == 0;
+        return t.size() == 0;
     }
 };
