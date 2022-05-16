b427dcb679181f33010bdfc141612957199678ac
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-16 15:33:00 +0100

    Time: 42 ms (23.62%), Space: 21.7 MB (8.98%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -5,10 +5,9 @@ public:
         unordered_map<char, int> rows[9], cols[9], boxes[9];
         
         for (int i = 0; i<r; i++){
-            for(int j = 0; j<c; j++){
+            for (int j = 0; j<c ; j++){
                 char c = board[i][j];
-                if (c != '.' && (rows[i][c]>0 || cols[j][c]>0 || boxes[i/3*3+j/3][c]>0))
-                    return false;
+                if (c!='.' && (rows[i][c]>0 || cols[j][c]>0 || boxes[i/3*3+j/3][c]>0)) return false;
                 rows[i][c]++;cols[j][c]++;boxes[i/3*3+j/3][c]++;
             }
         }return true;
