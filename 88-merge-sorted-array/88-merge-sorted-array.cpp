197106af70f49153b1bbce96a4ec119147455a82
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-17 07:04:32 +0100

    Time: 0 ms (100.00%), Space: 9.1 MB (73.09%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int final = m+n-1;
+        int i = n-1, j = m-1;
         
-        int last = m+n-1;
-        
-        while( m>0 && n>0 ){
-            
-            if (nums1[m-1]>nums2[n-1]){
-                nums1[last] = nums1[m-1];
-                last--;
-                m--;
+        while (i>=0 && j>=0){
+            if (nums1[j]>nums2[i]){
+                nums1[final] = nums1[j];
+                final--;j--;
             }
-            
             else{
-                nums1[last] = nums2[n-1];
-                last--;
-                n--;
+                nums1[final]=nums2[i];
+                final--; i--;
             }
         }
-        
-        while( n>0 ){
-            nums1[last] = nums2[n-1];
-            last--;
-            n--;
+        while(i>=0){
+            nums1[final] = nums2[i];
+            final--;i--;
         }
-        
     }
 };
