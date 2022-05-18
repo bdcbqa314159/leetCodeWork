6ac4186bf23a0ce0aee9ed8d4ec0870ca28765b4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-18 07:51:30 +0100

    Time: 4 ms (76.11%), Space: 9.6 MB (5.77%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,14 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r = matrix.size(), c = matrix[0].size(), a = 0, b = r-1, row = 0, mid = 0;
         while (a<=b){
-            row = a+(b-a)/2;
-            if (target < matrix[row][0]) b = row-1;
-            else if (target > matrix[row][c-1]) a = row+1;
+            row = a + (b-a)/2;
+            if (target<matrix[row][0]) b = row-1;
+            else if (target>matrix[row][c-1]) a = row+1;
             else break;
         }
         
         if (a>b) return false;
         a = 0; b = c-1;
+        
         while (a<=b){
             mid = a + (b-a)/2;
             int x = matrix[row][mid];
