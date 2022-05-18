b328b2e44996d1e837d65a08aa53ef2db1dac642
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-18 08:23:58 +0100

    Time: 13 ms (59.59%), Space: 11.6 MB (79.78%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -14,7 +14,7 @@ public:
         if (!head) return nullptr;
         ListNode* cur = head;
         while (cur->next){
-            if(cur->val == cur->next->val)
+            if (cur->val == cur->next->val)
                 cur->next = cur->next->next;
             else
                 cur = cur->next;
