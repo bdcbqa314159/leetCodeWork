01d3c572048eb99f0e673992d326d1c211934cf0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-19 15:53:06 +0100

    Time: 4 ms (22.64%), Space: 6.6 MB (33.71%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascal(numRows);
+        vector<vector<int>> r(numRows);
         for (int i = 0; i<numRows; i++){
-            pascal[i].resize(i+1);
-            pascal[i][0] = pascal[i][i] = 1;
-            for (int j = 1; j<i; j++)
-                pascal[i][j] = pascal[i-1][j-1]+pascal[i-1][j];
+            r[i].resize(i+1);
+            r[i][0] = r[i][i] = 1;
+            for (int j = 1; j<i; j++){
+                r[i][j] = r[i-1][j-1]+r[i-1][j];
+            }
+            
         }
-        return pascal;
+        return r;
     }
+    
 };
