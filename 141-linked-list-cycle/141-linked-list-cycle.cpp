00a3e51563131599e8567e7d6e3594bfbdf4695d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-19 16:27:17 +0100

    Time: 28 ms (9.93%), Space: 8 MB (58.67%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -10,7 +10,8 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (!head) return false;
-        ListNode *slow = head, *fast=head->next;
+        ListNode *slow=head, *fast=head->next;
+        
         while (slow!=fast){
             if (!fast || !fast->next)
                 return false;
