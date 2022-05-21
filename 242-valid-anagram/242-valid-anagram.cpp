5f9188ca9498a10d83c5c6df740ea8c886a33420
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-21 16:40:35 +0100

    Time: 9 ms (72.11%), Space: 7.4 MB (46.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     
     vector<int> helper(string s){
-        vector<int> x(26,0);
-        for (int i = 0; i<s.size(); i++){
-            x[s[i]-'a']++;
-        }
-        return x;
+        vector<int> o(26,0);
+        for (int i = 0; i<s.size(); i++)
+            o[s[i]-'a']++;
+        return o;
     }
     
     bool isAnagram(string s, string t) {
         int n = s.size(), m = t.size();
         if (n!=m) return false;
         vector<int> u = helper(s), v = helper(t);
-        for (int i = 0; i<26; i++){
-            if (v[i]!=u[i])
+        for (int i =0; i<u.size(); i++){
+            if (u[i] != v[i])
                 return false;
         }
         return true;
