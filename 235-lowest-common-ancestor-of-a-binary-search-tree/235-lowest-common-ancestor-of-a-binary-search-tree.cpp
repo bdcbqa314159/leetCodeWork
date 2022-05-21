8fc899204ea454320023bcde6f05f5de9e0aa8df
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-21 16:23:32 +0100

    Time: 36 ms (64.12%), Space: 23.2 MB (63.46%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         int x = root->val, u = p->val, v = q->val;
-        if (u<x && v<x)
+        if (u<x && v<x) 
             return lowestCommonAncestor(root->left, p, q);
         else if (u>x && v>x)
             return lowestCommonAncestor(root->right, p, q);
