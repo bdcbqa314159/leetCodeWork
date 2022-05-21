ede599bc6352062c3fcd127f14c4c9190ce66758
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-21 21:44:53 +0100

    Time: 6 ms (70.36%), Space: 10.7 MB (21.87%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
         if (m>n) return intersect(nums2, nums1);
-        
         unordered_map<int,int> umap;
         for (int i = 0; i<n; i++){
-            if (umap.find(nums1[i])!=umap.end())
+            if (umap.find(nums1[i]) != umap.end())
                 umap[nums1[i]]++;
             else
                 umap[nums1[i]] = 1;
         }
         vector<int> ans;
-        for (int i = 0;i<m; i++){
+        for (int i = 0; i<m; i++){
             if (umap[nums2[i]]>0){
                 ans.push_back(nums2[i]);
                 umap[nums2[i]]--;
