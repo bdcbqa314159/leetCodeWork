caf42a33f3e75e6d78cd7f118297aa83bc8ab20f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-21 16:51:09 +0100

    Time: 4 ms (94.38%), Space: 7.4 MB (46.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,7 +3,7 @@ public:
     
     vector<int> helper(string& s){
         vector<int> z(26,0);
-        for (int i =0; i<s.size(); i++)
+        for (int i = 0; i<s.size(); i++)
             z[s[i]-'a']++;
         return z;
     }
@@ -12,8 +12,7 @@ public:
         int n = s.size(), m = t.size();
         if (n!=m) return false;
         vector<int> u = helper(s), v = helper(t);
-        
-        for (int i =0; i<26; i++){
+        for (int i = 0; i<26 ; i++){
             if (u[i]!=v[i])
                 return false;
         }
