606472baa919b6bfc1286274efe44b417c0ea368
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-22 13:37:28 +0100

    Time: 8 ms (91.55%), Space: 11.1 MB (31.70%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int n = mat.size(), m = mat[0].size();
-        if (m*n != r*c) return mat;
-        
+        if (n*m != r*c) return mat;
         vector<int> v;
         vector<vector<int>> ans(r, vector<int>(c,0));
         
@@ -12,12 +11,13 @@ public:
                 v.push_back(mat[i][j]);
             }
         }
-        int k= 0;
+        
+        int k = 0;
         for (int i = 0; i<r; i++){
             for (int j = 0; j<c; j++){
-                ans[i][j] = v[k];
-                k++;
+                ans[i][j] = v[k]; k++;
             }
-        }return ans;
+        }
+        return ans;
     }
 };
