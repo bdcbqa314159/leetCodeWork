34cfdd66aac1ec575dff9b645733a3b6045285b8
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-22 10:05:37 +0100

    Time: 13 ms (82.04%), Space: 8.9 MB (38.81%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     vector<int> helper(string &s){
-        vector<int>ans(26,0);
+        vector<int> ans(26,0);
         for (auto x:s)
             ans[x-'a']++;
         return ans;
@@ -11,9 +11,8 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         int n = ransomNote.size(), m = magazine.size();
         if (n>m) return false;
-        
         vector<int> u = helper(ransomNote), v = helper(magazine);
-        for (int i =0; i<26; i++){
+        for (int i = 0; i<26; i++){
             if (u[i]>v[i])
                 return false;
         }
