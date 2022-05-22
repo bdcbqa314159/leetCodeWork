e66891d74239e79896c8a3915c08f0a49231e72c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-22 14:46:25 +0100

    Time: 155 ms (14.44%), Space: 56.8 MB (99.27%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -12,14 +12,13 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if (!root){
+        if (!root) {
             TreeNode *node = new TreeNode(val);
             return node;
         }
-        if (val<root->val)
-            root->left = insertIntoBST(root->left, val);
-        else 
-            root->right = insertIntoBST(root->right, val);
+        
+        if (val<root->val) root->left = insertIntoBST(root->left, val);
+        else root->right = insertIntoBST(root->right, val);
         return root;
     }
 };
