dc7e28432a60a46d2faa48f3dd168df01ca8a05b
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-22 13:42:46 +0100

    Time: 72 ms (23.71%), Space: 36.8 MB (80.40%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,25 +11,29 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& ans){
+    
+    void helper(TreeNode* root, vector<int>& x){
         if (!root) return;
-        helper(root->left, ans);
-        ans.push_back(root->val);
-        helper(root->right, ans);
+        helper(root->left, x);
+        x.push_back(root->val);
+        helper(root->right, x);
     }
     
-    vector<int> inOrderTraversal(TreeNode* root){
-        vector<int> ans;
-        helper(root, ans);
-        return ans;
+    vector<int> inOrder(TreeNode* root){
+        if (!root) return {};
+        vector<int> out;
+        helper(root, out);
+        return out;
     }
+    
     
     bool findTarget(TreeNode* root, int k) {
         if (!root) return false;
-        vector<int> ans = inOrderTraversal(root);
-        int l = 0, r = ans.size()-1;
+        vector<int> order = inOrder(root);
+        int l = 0, r = order.size()-1;
+        
         while (l<r){
-            int sum = ans[l]+ans[r];
+            int sum = order[l]+order[r];
             if (sum == k) return true;
             else if (sum<k) l++;
             else r--;
