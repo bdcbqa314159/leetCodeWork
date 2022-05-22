585ad2615fb740b4f7cbbe084c16bffde4efe143
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-22 14:25:28 +0100

    Time: 50 ms (33.53%), Space: 34.9 MB (28.05%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if (!root || root->val == val) return root;
-        else if (root->val < val) return searchBST(root->right, val);
+        else if (root->val<val) return searchBST(root->right, val);
         else return searchBST(root->left, val);
     }
 };
