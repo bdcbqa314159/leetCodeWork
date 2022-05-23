f514f9bdcf23a7c4958696b0bf99b02af8096a24
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-24 00:41:03 +0100

    Time: 104 ms (75.57%), Space: 59 MB (39.06%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -3,9 +3,11 @@ public:
     int maxArea(vector<int>& height) {
         int l = 0, r = height.size()-1, max_ = 0, area = 0;
         while (l<r){
-            area = min(height[r],height[l])*(r-l);
-            max_ = max(area, max_);
-            height[r]>height[l]?l++:r--;
-        }return max_;
+            area = min(height[r], height[l])*(r-l);
+            max_ = max(max_, area);
+            if (height[r]>height[l]) l++;
+            else r--;
+        }
+        return max_;
     }
 };
