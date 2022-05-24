95abbaa2ca854b4ddf5f68492ff0257b65f729bd
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-05-24 01:42:53 +0100

    Time: 8 ms (37.09%), Space: 7.2 MB (67.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-//         int maxLen = 0, lastError = -1;
-//         vector<int> stack;
+        int maxLen = 0, lastError = -1;
+        vector<int> stack;
         
-//         for (int i = 0; i<s.size(); i++){
-//             if (s[i] == '(') stack.push_back(s[i]);
-//             else if(s[i] == ')'){
-//                 if (stack.size()>0){
-//                     stack.pop_back();
-//                     int len;
-//                     if (stack.size() == 0)
-//                         len = i-lastError;
-//                     else{
-//                         len = i-stack.back();
-//                     }
-//                     if (len>maxLen) maxLen = len;
-//                 }else
-//                     lastError = i;
-//             }
-//         }
-//         return maxLen;
-         int maxLen = 0;
-    int lastError = -1;
-    vector<int> stack;
-    for(int i=0; i<s.size(); i++){
-        if (s[i] == '('){
-            stack.push_back(i);
-        }else if (s[i] == ')') {
-            if (stack.size()>0 ){
-                stack.pop_back();
-                int len;
-                if (stack.size()==0){
-                    len = i - lastError;
-                } else {
-                    len = i - stack.back();
-                }
-                if (len > maxLen) {
-                    maxLen = len;
-                }
-            }else{
-                lastError = i;
+        for (int i = 0; i<s.size(); i++){
+            if (s[i] == '(') stack.push_back(i);
+            else if(s[i] == ')'){
+                if (stack.size()>0){
+                    stack.pop_back();
+                    int len;
+                    if (stack.size() == 0)
+                        len = i-lastError;
+                    else{
+                        len = i-stack.back();
+                    }
+                    if (len>maxLen) maxLen = len;
+                }else
+                    lastError = i;
             }
         }
-    }
-    return maxLen;
+        return maxLen;
+    
     }
 };
