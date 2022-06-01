b64e7a87d25043173258979825a1cd41e66a741b
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-06-01 08:54:56 +0100

    Time: 14 ms (74.80%), Space: 8.2 MB (73.48%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,14 @@ public:
         int n = s.size(), ans = 0;
         if (n == 0) return ans;
         
-        int l = 0,r = 0;
+        int l = 0, r = 0;
         unordered_map<char, int> umap;
         while (l<n && r<n){
             char e = s[r];
-            if(umap.find(e) != umap.end())
+            if (umap.find(e)!=umap.end())
                 l = max(l, umap[e]+1);
             ans = max(ans, r-l+1);
-            umap[e] = r;
+            umap[e]=r;
             r++;
         }return ans;
     }
