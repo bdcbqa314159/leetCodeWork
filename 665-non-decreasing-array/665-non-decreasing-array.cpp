5fb4398a60d7f773befd128ee2e04ead77f3880c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-06-25 15:22:41 +0100

    Time: 32 ms (78.06%), Space: 27.1 MB (12.14%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int counter = 0;
+        for (int i = 1; i<nums.size(); i++){
+            if (nums[i]<nums[i-1]){
+                if (i == 1 || nums[i-2] <= nums[i]){
+                    nums[i-1] = nums[i];
+                    counter ++;
+                }
+                else{
+                    nums[i] = nums[i-1];
+                    counter++;
+                }
+            }
+        }
+        return counter<=1;
+        
+    }
+};
+
