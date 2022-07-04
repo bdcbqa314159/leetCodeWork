d99b97adfd7f9b552b05c9faae14a4f5fb3bc999
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-04 17:48:01 +0100

    Time: 0 ms (100.00%), Space: 7.7 MB (55.20%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+    
     void solve(vector<vector<int>> &ans, vector<int> &nums, int index){
         if (index == nums.size()){
             ans.push_back(nums);
             return;
         }
         for (int i = index; i<nums.size(); i++){
-            swap(nums[index], nums[i]);
+            swap(nums[i], nums[index]);
             solve(ans, nums, index+1);
-            swap(nums[index], nums[i]);
+            swap(nums[i], nums[index]);
         }
         return;
     }
-    
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
