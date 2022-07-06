4a0394acb7f735a5642806d63fe50b764612c3cf
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-06 09:53:02 +0100

    Time: 0 ms (100.00%), Space: 5.8 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n<=1) return n;
+        int a = 0, b = 1, c = 0;
+        
+        for (int i = 2; i<n+1; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
