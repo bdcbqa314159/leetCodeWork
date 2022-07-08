b36f188337070c4139f992a0994ab022ddfaf82c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-08 08:14:02 +0100

    Time: 15 ms (15.70%), Space: 16.4 MB (48.35%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -13,15 +13,17 @@ class Solution {
 public:
     
     bool helper(TreeNode* l, TreeNode* r){
+        
         if (!l && !r) return true;
-        if (!l && r) return false;
-        if (l&& !r) return false;
+        if ( (!l && r) || (!r && l)) return false;
         if (l->val != r->val) return false;
+        
         return helper(l->left, r->right)&&helper(l->right, r->left);
+        
     }
     
     bool isSymmetric(TreeNode* root) {
         if (!root) return true;
-        return helper(root->left,root->right);
+        return helper(root->left, root->right);
     }
 };
