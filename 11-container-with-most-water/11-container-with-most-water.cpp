72f8da088c6f564d28a9b4417aab34e82c9db4b5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-08 06:59:41 +0100

    Time: 142 ms (45.65%), Space: 59 MB (80.77%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,10 +4,11 @@ public:
         int l = 0, r = height.size()-1, area = 0, maxArea = 0;
         while (l<r){
             area = min(height[l], height[r])*(r-l);
-            maxArea = max(area,maxArea);
+            maxArea = max(area, maxArea);
             if (height[l]<height[r]) l++;
             else r--;
         }
+        
         return maxArea;
     }
 };
