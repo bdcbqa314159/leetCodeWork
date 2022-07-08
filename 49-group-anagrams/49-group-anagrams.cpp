e61b15e89400953fe92bfa767a0f3278f512cac0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-08 07:25:43 +0100

    Time: 66 ms (38.73%), Space: 19.6 MB (74.99%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -6,17 +6,14 @@ public:
         unordered_map<string, vector<string>> umap;
         unordered_map<string, vector<string>>::iterator it;
         
-        for (string str:strs){
+        for (string str: strs){
             key = str;
             sort(key.begin(), key.end());
-            
             umap[key].push_back(str);
         }
         
         for (it = umap.begin(); it!=umap.end(); it++)
             answer.push_back(it->second);
-        
         return answer;
-        
     }
 };
