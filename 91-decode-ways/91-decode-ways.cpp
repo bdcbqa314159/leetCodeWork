3a85becaae3bb4fbf0d34f85aedc7488486aedf2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-07-18 19:54:11 +0100

    Time: 0 ms (100.00%), Space: 6.2 MB (72.91%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -3,14 +3,14 @@ public:
     int numDecodings(string s) {
         int n = s.size();
         if (n == 0 || s[0] == '0') return 0;
-        if (n ==1 ) return 1;
-        int d1,d2,v, v1 = 1, v2 = 1;
-        for (int i = 1; i<n; i++){
+        if (n == 1) return 1;
+        int v,d1,d2, v1 = 1, v2 = 1;
+        for (int i =1; i<n; i++){
             v = 0;
             d1 = s[i]-'0';
-            d2 = 10*(s[i-1]-'0')+d1;
-            if (d1>=1) v+=v1;
-            if (d2<=26 && d2>=10) v+=v2;
+            d2 = d1+10*(s[i-1]-'0');
+            if (d1 >= 1) v += v1;
+            if (d2 <= 26 && d2 >= 10) v+=v2;
             v2 = v1;
             v1 = v;
         }
