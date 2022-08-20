d3650cff566cf3f2b1cfba5fc31faca48b52049e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-08-20 23:36:54 +0100

    Time: 232 ms (76.01%), Space: 109.7 MB (9.01%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double sum = accumulate(nums.begin(), nums.begin() + k, 0);
+        double sum = accumulate(nums.begin(), nums.begin()+k, 0);
         double res = sum/k;
-        for (int i = 0; i < nums.size() - k; i++) {
-            sum = sum - nums[i] + nums[i+k];
+        
+        for (int i = 0;i< nums.size()-k; i++){
+            sum = sum-nums[i]+nums[i+k];
             res = max(res, sum/k);
         }
+        
         return res;
     }
 };
