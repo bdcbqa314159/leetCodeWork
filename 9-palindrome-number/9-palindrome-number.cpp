014eb3b4f509e09f24563dd25977bd0cecfa8fcf
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-08-20 18:00:41 +0100

    Time: 20 ms (61.27%), Space: 5.8 MB (91.05%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     
     long int helper(int x){
+    
         long int r = 0;
         while (x>0){
-            r*=10;
-            r+= x%10;
-            x/=10;
+            int unit = x%10;
+            x /= 10;
+            r = 10*r+unit;
         }
+        
         return r;
+    
     }
     
     bool isPalindrome(int x) {
         if (x<0) return false;
-        return x==helper(x);
+        return x == helper(x);
     }
 };
