573ebddc1e18b952fbfb1b8e9d6da75a6985d425
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-08-25 09:02:56 +0100

    Time: 0 ms (100.00%), Space: 7.2 MB (22.54%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *slow, *fast;
+        ListNode *slow = head, *fast=head;
         while (fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
