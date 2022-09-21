4ce63cd85eb1c485550914fe17dc12db184b53b0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-09-21 14:01:37 +0100

    Time: 7 ms (60.77%), Space: 73.3 MB (35.05%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        
+        int[] output_arr = new int[queries.length];
+        
+        int even_sum = 0;
+        
+        for (int val : nums){
+            if (val%2 ==0){
+                even_sum+=val;
+            }
+        }
+    
+        for (int i = 0; i<queries.length; i++){
+            int index = queries[i][1];
+            int value = queries[i][0];
+            
+            if (nums[index]%2 == 0){
+                even_sum-=nums[index];
+            }
+        
+            nums[index]+=value;
+            if (nums[index]%2 == 0){
+                even_sum+=nums[index];
+            }
+            output_arr[i] = even_sum;
+        }
+        return output_arr;
+    }
+}
