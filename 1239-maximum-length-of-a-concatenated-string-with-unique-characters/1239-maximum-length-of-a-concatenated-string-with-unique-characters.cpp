07e382e07cf3125088f84d0a01ab05d9291a5094
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-10-24 23:05:56 +0100

    Time: 7 ms (92.09%), Space: 9 MB (78.58%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -4,22 +4,25 @@ public:
     vector<int> a;
     
     for (const string& x : arr) {
-      set<char> s(begin(x), end(x));
-      if (s.size() != x.length()) continue;
-      a.push_back(0);      
-      for (char c : x) a.back() |= 1 << (c - 'a');      
+      int mask = 0;      
+      for (char c : x) mask |= 1 << (c - 'a');
+      if (__builtin_popcount(mask) != x.length()) continue;
+      a.push_back(mask);
     }
     
     int ans = 0;
     
-    function<void(int, int)> dfs = [&](int s, int cur) {
-      ans = max(ans, __builtin_popcount(cur));
-      for (int i = s; i < a.size(); ++i)
-        if ((cur & a[i]) == 0)
-          dfs(i + 1, cur | a[i]);      
-    };
+    vector<int> dp{0};
+    for (int i = 0; i < a.size(); ++i) {
+      int size = dp.size();
+      for (int j = 0; j < size; ++j) {
+        if (dp[j] & a[i]) continue;
+        int t = dp[j] | a[i];        
+        dp.push_back(t);
+        ans = max(ans, __builtin_popcount(t));
+      }
+    }
     
-    dfs(0, 0);
     return ans;
   }
 };
