80b871f27f16fd550a723532720bf482a1f5d456
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-18 18:41:10 +0000

    Time: 32 ms, Memory: 17.6 MB - LeetHub

---
+++ diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        if (mat.empty() || mat.front().empty()) return {};
+        
+        int m = mat.size();
+        int n = mat.front().size();
+        
+        int total = m*n;
+        
+        vector<int> ans(total);
+        
+        bool up = true;
+        
+        int i = 0, j = 0;
+        
+        for (int k = 0; k<total; k++){
+            ans[k] = mat[i][j];
+            
+            if (up && (i == 0 || j == n-1)){
+                if (j == n-1) i++;
+                else j++;
+                up = !up;
+            }
+            
+            else if(!up && (j == 0 || i == m-1)){
+                if (i == m-1) j++;
+                else i++;
+                up = !up;
+            }
+            else{
+                if (up){i--; j++;}
+                else{i++; j--;}
+            }
+        }
+        return ans;
+    }
+};
