f5de66f81a3e9ae276687ac2322f90ba84d143b3
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-22 12:32:20 +0000

    Time: 5 ms (57.24%), Space: 6.4 MB (56.58%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string result;
-        int l1 = a.size()-1;
-        int l2 = b.size()-1;
-        
+        string result{};
+        int l1 = a.size()-1, l2 = b.size()-1;
         int carry = 0;
         
-        while (l1>=0 || l2>=0 || carry > 0){
+        while (l1 >= 0 || l2 >= 0 || carry>0){
             
-            int val1 = 0;
-            int val2 = 0;
+            int v1 = 0;
+            int v2 = 0;
+            int sum = 0;
             
-            val1 = (l1>=0) ? a[l1] - '0':0;
-            val2 = (l2>=0) ? b[l2] - '0':0;
+            v1 = (l1>=0) ?  a[l1]-'0':0;
+            v2 = (l2>=0) ?  b[l2]-'0':0;
             
-            int sum = val1+val2+carry;
+            sum = carry+v1+v2;
             
-            result.push_back((sum&1)+'0');
+            result.push_back((sum&1) + '0');
             carry = (sum>>1)&1;
             
-            l1--;l2--;
+            l1--; l2--;
             
         }
-        
         reverse(result.begin(), result.end());
         return result;
     }
 };
-
-
-
