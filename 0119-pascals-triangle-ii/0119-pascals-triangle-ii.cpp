7c5f4d2f9b180f58fbaba75fc4ea07237a49fa7b
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-22 22:19:47 +0000

    Time: 6 ms (15.41%), Space: 6.4 MB (79.19%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> result(rowIndex+1, 1);
+        vector<int> result(rowIndex+1,1);
         for (int i = 0; i<rowIndex; i++){
             for (int j = i; j>0; j--){
                 result[j]+=result[j-1];
