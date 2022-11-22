bdb1b88d3600222322f1268d4b9ca985a333d2ee
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-22 22:56:24 +0000

    Time: 18 ms (81.51%), Space: 15.7 MB (44.46%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size()-1;
+        vector<int> answer(2);
+        while (l<r){
+            int ans = numbers[l]+numbers[r];
+            if (target == ans){
+                answer[0] = l+1;
+                answer[1] = r+1;
+                break;
+            }
+            else if(ans<target) l++;
+            else r--;
+        }
+        return answer;
+    }
+};
