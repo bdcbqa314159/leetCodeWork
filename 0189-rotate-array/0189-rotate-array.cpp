cc0d0b1dfcc9c875bfb47cb45fa8a95f2c0cb0a7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-22 23:16:29 +0000

    Time: 39 ms (82.12%), Space: 24.8 MB (93.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -9,11 +9,14 @@ public:
         return;
     }
     
+    
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k%=n;
         helper(nums, 0, n-1);
         helper(nums, 0, k-1);
         helper(nums, k, n-1);
+        return;
     }
+    
 };
