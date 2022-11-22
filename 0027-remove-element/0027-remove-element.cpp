d0ad3aefa94fdced44a630fe7821cfcc570d6008
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-22 09:45:25 +0000

    Time: 3 ms (70.53%), Space: 8.9 MB (36.65%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int position = 0;
+        for (int n: nums){
+            if (n!=val){
+                nums[position] = n;
+                position++;
+            }
+        }
+        return position;
+    }
+};
