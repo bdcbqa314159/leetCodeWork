e86e9a2602842f66ec51a5d395f41118e5b6683a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-27 17:25:46 +0000

    Time: 9 ms (29.17%), Space: 6.9 MB (26.81%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> visited(26, false);
+        vector<int> count(26,0);
+        
+        stack<char> temp_answers;
+        
+        for (char ch:s)
+            count[ch-'a']++;
+        
+        for (char ch: s){
+            int index = ch-'a';
+            count[index]--;
+            
+            if (visited[index]) continue;
+            
+            while (!temp_answers.empty() && temp_answers.top()>ch && count[temp_answers.top()-'a']>0){
+                char c = temp_answers.top();
+                temp_answers.pop();
+                visited[c-'a'] = false;
+            }
+            if (visited[index] == false){
+                temp_answers.push(ch);
+                visited[index] = true;
+            }
+        }
+        string res("");
+        while (!temp_answers.empty()){
+            res = temp_answers.top()+res;
+            temp_answers.pop();
+        }
+        return res;
+    }
+};
