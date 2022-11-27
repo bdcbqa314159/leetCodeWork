e19fc5dc0ad9670541461e59fde139de79019bb1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-27 17:39:07 +0000

    Time: 13 ms (11.38%), Space: 6.8 MB (42.69%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<bool> visited(26, false);
-        vector<int> count(26,0);
+        vector<int> freq(26,0);//Track last index of occurence for each element
+        for(int i=0;i<s.size();++i)
+            freq[s[i]-'a']++;
         
-        stack<char> temp_answers;
-        
-        for (char ch:s)
-            count[ch-'a']++;
-        
-        for (char ch: s){
-            int index = ch-'a';
-            count[index]--;
-            
-            if (visited[index]) continue;
-            
-            while (!temp_answers.empty() && temp_answers.top()>ch && count[temp_answers.top()-'a']>0){
-                char c = temp_answers.top();
-                temp_answers.pop();
-                visited[c-'a'] = false;
+        stack<char> st;//Monotonic stack to maintain increasing order of chars
+        vector<bool> seen(26,false);//Track already included elements
+        for(int i=0;i<s.size();++i){
+            if(seen[s[i]-'a']){//Don't process already included char
+                freq[s[i]-'a']--;
+                continue;
             }
-            if (visited[index] == false){
-                temp_answers.push(ch);
-                visited[index] = true;
+            while(!st.empty() and st.top()>s[i] and freq[st.top()-'a']>0){//Pop all possible larger chars
+                seen[st.top()-'a']=false;
+                st.pop();
             }
+            st.push(s[i]);
+            seen[s[i]-'a']=true;
+            freq[s[i]-'a']--;
         }
-        string res("");
-        while (!temp_answers.empty()){
-            res = temp_answers.top()+res;
-            temp_answers.pop();
+        string ans="";
+        while(!st.empty()){//Build answer string
+            ans.push_back(st.top());
+            st.pop();
         }
-        return res;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
