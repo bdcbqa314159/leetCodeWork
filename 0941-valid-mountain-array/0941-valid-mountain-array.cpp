222f555ac636c2dcf4be08736c800917ada31306
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 15:48:26 +0000

    Time: 99 ms (6.03%), Space: 22.5 MB (48.20%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& a) {
-        int i = 0, n = a.size();
+        int n = a.size(), i = 0;
         while (i<n && i+1<n && a[i]<a[i+1])
             i++;
-        if (i == 0 || i >= n-1) return false;
+        if (i == 0 || i>=n-1) return false;
         
-        while (i<n && i+1 < n){
+        while (i<n && i+1<n){
             if (a[i]<=a[i+1]) return false;
             i++;
         }
+        
         return true;
     }
 };
