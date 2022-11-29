4c0e50751efc82a1d77f650799235adbcf164b65
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 15:22:31 +0000

    Time: 19 ms (62.86%), Space: 16.3 MB (58.35%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int j = 0;
-        for (int i = 0; i<nums.size(); i++){
+        int j = 0, n = nums.size();
+        for (int i = 0; i<n; i++){
             if (nums[i]%2 == 0){
-                swap(nums[i], nums[j]); j++;
-            }    
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
         return nums;
     }
