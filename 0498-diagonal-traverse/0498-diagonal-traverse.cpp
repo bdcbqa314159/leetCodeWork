b6a6720dc84dcf37a3c31c5bd7b129b369b69d0c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 11:14:57 +0000

    Time: 76 ms (47.85%), Space: 17.8 MB (92.99%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        if (mat.empty()) return {};
-        if (mat[0].empty()) return {};
+        if (mat.size()==0) return {};
+        if (mat[0].size()==0) return {};
         
-        int m = mat.size(), n = mat[0].size();
+        int m = mat.size();
+        int n = mat[0].size();
+        
         int total = m*n;
-        
         vector<int> ans(total);
-        
+        bool up = true;
         int i = 0, j = 0;
         
-        bool up = true;
-        
         for (int k = 0; k<total; k++){
-            
             ans[k] = mat[i][j];
             
-            if (up && (i==0 || j == n-1)){
+            if (up && (i == 0 || j == n-1)){
                 if (j == n-1) i++;
                 else j++;
                 up = !up;
             }
             
-            else if(!up && (j==0 || i == m-1)){
-                if (i==m-1) j++;
+            else if(!up && (j == 0 || i == m-1)){
+                if (i == m-1) j++;
                 else i++;
                 up = !up;
             }
             
             else{
                 if (up) {i--; j++;}
-                else {j--; i++;}
+                else{i++;j--;}
             }
-                
+            
         }
         
         return ans;
