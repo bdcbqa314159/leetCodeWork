378e7d90bc2d4d57afc3c8f7986d91cbc17e4b45
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 16:18:07 +0000

    Time: 7 ms (32.43%), Space: 8.3 MB (24.73%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         vector<int> arr(heights);
-        int n = heights.size();
         sort(arr.begin(), arr.end());
-        int count = 0;
-        for (int i = 0; i<n; i++)
-            if(arr[i]!=heights[i]) count++;
+        int n = heights.size(), count = 0;
+        for (int i =0; i<n; i++)
+            if (arr[i]!=heights[i]) count++;
         return count;
-    }   
+    }
 };
