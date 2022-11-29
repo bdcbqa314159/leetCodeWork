364083c0d936814379af6ea6a0efcef8c9f09ed3
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 16:38:42 +0000

    Time: 228 ms (29.62%), Space: 68.6 MB (11.48%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int rightMax = -1;
-        int n = arr.size();
-        
+        int rightMax = -1, newMax = 0, n = arr.size();
         for (int i = n-1; i>=0; i--){
-            int newMax = max(rightMax, arr[i]);
+            newMax = max(rightMax, arr[i]);
             arr[i] = rightMax;
             rightMax = newMax;
         }
