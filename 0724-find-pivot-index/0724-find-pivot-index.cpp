f23e9f68e1f9bfa0e4488c7c0d1aa199159c14fc
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 12:02:01 +0000

    Time: 67 ms (24.46%), Space: 31.1 MB (51.93%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size(), total = 0;
-        for (auto n: nums) total += n;
-        
+        for (auto n: nums) total+=n;
         int left = 0;
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i<n; i++){
             if (i!=0) left += nums[i-1];
             if (total - nums[i] == 2*left) return i;
-        }
-        return -1;
+        }return -1;
     }
 };
