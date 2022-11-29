da9ecfa16c93d8e0b8245987c3174eb0f59b9a91
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 15:17:34 +0000

    Time: 7 ms (45.05%), Space: 10.9 MB (14.15%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int ans = INT_MIN, idx = 0, n = nums.size();
-        
+        int idx = 0, ans = INT_MIN, n = nums.size();
         for (int i = 0; i<n; i++){
             if (nums[i]>ans){
                 idx = i;
                 ans = nums[i];
             }
         }
-        
         for (int i = 0; i<n; i++){
-            if (ans != nums[i] && ans<2*nums[i]) return -1;
+            if (ans!=nums[i] && ans<2*nums[i]) return -1;
         }
-        return idx;
         
+        return idx;
     }
 };
