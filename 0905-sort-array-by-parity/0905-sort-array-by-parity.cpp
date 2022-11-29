0a405add00597eef73e76cc008c341b376235a1d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 15:26:55 +0000

    Time: 23 ms (46.42%), Space: 16.2 MB (58.35%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size(), j = 0;
         for (int i = 0; i<n; i++){
             if (nums[i]%2 == 0){
-                swap(nums[i], nums[j]); j++;
+                swap(nums[i], nums[j]);j++;
             }
         }
         return nums;
