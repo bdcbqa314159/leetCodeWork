742ea4ae5acab4482aaeae47479030fda97f1085
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-29 16:27:44 +0000

    Time: 201 ms (16.50%), Space: 9.7 MB (78.16%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -3,15 +3,17 @@ public:
     
     void helper(int j, vector<int>& a){
         int n = a.size();
-        for(int i = n-1; i>j; i--)
-            a[i]=a[i-1];
+        for (int i = n-1; i>j; i--){
+            a[i] =a[i-1];
+        }
+        
         return;
     }
     
     void duplicateZeros(vector<int>& arr) {
         int n = arr.size();
         for (int i = 0; i<n; i++){
-            if (arr[i]==0){
+            if (arr[i] == 0){
                 helper(i, arr);
                 i++;
             }
