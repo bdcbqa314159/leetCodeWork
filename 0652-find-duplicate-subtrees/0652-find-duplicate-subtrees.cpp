1f31df289893f4765807578282c1b3b950a23b20
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-02-28 22:53:07 +0000

    Time: 31 ms (66.97%), Space: 27.8 MB (88.76%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        vector<TreeNode*> result;
+        map<size_t, vector<TreeNode*>> mymap;
+        merkle(root, mymap);
+        for (const auto& ele : mymap) {
+            if (ele.second.size() > 1) {
+                result.push_back(ele.second[0]);
+            }
+        }
+        return result;
+    }
+    
+    string merkle(TreeNode* root, map<size_t, vector<TreeNode*>>& mymap) {
+        if (root == nullptr) {
+            return "#";
+        }
+        string left = merkle(root->left, mymap);
+        string right = merkle(root->right, mymap);
+        size_t current = hash<string>{}(left + to_string(root->val) + right);
+        mymap[current].push_back(root);
+        return to_string(current);
+    }
+};
