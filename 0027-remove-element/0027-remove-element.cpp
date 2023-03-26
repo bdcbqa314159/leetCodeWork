665ac27da3ed99ce68c8b29d00c6b25af9ca60d1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-26 18:10:10 +0100

    Time: 3 ms (59.13%), Space: 8.8 MB (47.41%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int position = 0, n = nums.size();
-        for (int i = 0; i< n; i++){
-            if (nums[i]!=val){
+        for (int i = 0; i<n; i++){
+            if (nums[i] != val){
                 nums[position] = nums[i];
                 position++;
             }
