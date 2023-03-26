dcb35c72c29d995fc71c43f97f225d7e9e6007a7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-26 18:40:52 +0100

    Time: 0 ms (100.00%), Space: 8.8 MB (68.83%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        
         for (int i = n-1; i>=0; i--){
-            if (digits[i] < 9){
+            if (digits[i]<9){
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
         }
+        
         digits.resize(n+1);
         digits[0] = 1;
         return digits;
