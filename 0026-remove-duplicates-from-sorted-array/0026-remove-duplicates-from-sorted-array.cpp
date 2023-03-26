85935c89c6e271a12aa7a56752921549e4699eba
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-26 17:59:50 +0100

    Time: 15 ms (51.33%), Space: 18.4 MB (79.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int position = 0, n = nums.size();
         for (int i = 0; i<n; i++){
-            if (position == 0 || nums[i] > nums[position-1]){
+            if (position == 0 || nums[i]>nums[position-1]){
                 nums[position] = nums[i];
                 position++;
             }
