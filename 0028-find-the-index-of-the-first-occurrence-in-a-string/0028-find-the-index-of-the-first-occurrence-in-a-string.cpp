8cacf7671d7bd8137def1343f721382ee2f38db6
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-26 18:13:02 +0100

    Time: 0 ms (100.00%), Space: 6.2 MB (95.55%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0; i<m-n+1; i++){
             int j = 0;
             while (j<n){
-                if (haystack[i+j]!=needle[j]) break;
+                if (haystack[i+j] != needle[j]) break;
                 j++;
             }
             if (j == n) return i;
