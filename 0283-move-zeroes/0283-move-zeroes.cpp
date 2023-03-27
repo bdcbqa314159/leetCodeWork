a18b0791ca30953246ac83ea69d2e5390afa96a4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-27 17:01:25 +0100

    Time: 27 ms (49.15%), Space: 19.4 MB (17.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j = 0, n = nums.size();
         for (int i = 0; i<n; i++){
-            if (nums[i]!=0){
+            if (nums[i] != 0){
                 nums[j] = nums[i];
                 j++;
             }
