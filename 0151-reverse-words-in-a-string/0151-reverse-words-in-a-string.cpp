b65487adf55f75c9eee6c0dbcd1e9a133372fca4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-27 15:20:28 +0100

    Time: 5 ms (58.41%), Space: 20.8 MB (23.24%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,18 +3,17 @@ public:
     string reverseWords(string s) {
         string result;
         int i = 0, n = s.size();
-        
-        while (i<n){
-            while (i<n && s[i]==' ')i++;
+        while(i<n){
+            while(i<n && s[i] == ' ') i++;
             if (i>=n) break;
             int j = i+1;
-            while (j<n && s[j]!=' ')j++;
+            while(j<n && s[j] != ' ') j++;
             string sub = s.substr(i, j-i);
             if (result.size() == 0) result = sub;
-            else result = sub +" "+result;
-            
+            else result = sub+" "+result;
             i = j+1;
         }
+        
         return result;
     }
 };
