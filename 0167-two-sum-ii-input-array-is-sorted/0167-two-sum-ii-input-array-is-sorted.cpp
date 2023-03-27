105d8f96e214a1af06251ea1db43b80f553cc36b
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-27 16:20:07 +0100

    Time: 11 ms (84.94%), Space: 15.7 MB (8.23%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,11 +6,11 @@ public:
         while (l<r){
             int sum = numbers[l]+numbers[r];
             if (target == sum){
-                ans[0]=l+1;
-                ans[1]=r+1;
+                ans[0] = l+1;
+                ans[1] = r+1;
                 break;
             }
-            else if (sum<target) l++;
+            else if(sum < target) l++;
             else r--;
         }
         return ans;
