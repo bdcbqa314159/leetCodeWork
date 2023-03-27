be37424f92519bd8780e0699cc963ccf30459079
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-27 16:55:20 +0100

    Time: 31 ms (94.84%), Space: 28.3 MB (14.42%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,17 +4,18 @@ public:
         int i = 0, j = 0, sum = 0, len = INT_MAX;
         nums.push_back(0);
         int n = nums.size();
+        
         while (j<n){
             if (sum>=target){
                 len = min(len, j-i);
                 sum-=nums[i];
                 i++;
             }
+            
             else{
                 sum+=nums[j];
                 j++;
             }
-        }
-        return len == INT_MAX? 0:len;
+        }return len==INT_MAX? 0: len;
     }
 };
