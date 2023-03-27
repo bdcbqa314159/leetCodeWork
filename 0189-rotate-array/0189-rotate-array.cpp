98f7a19416585f93fa3fb9375d80e4126a889b6f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-27 16:32:24 +0100

    Time: 33 ms (57.81%), Space: 25.1 MB (61.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     
-    void helper(vector<int>&nums, int l, int r){
+    void helper(vector<int>& nums, int l, int r){
         while (l<=r){
-            swap(nums[l], nums[r]);l++;r--;
+            swap(nums[l], nums[r]); l++; r--;
         }
         return;
     }
     
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k%=n;
+        k %= n;
+        
         helper(nums, 0, n-1);
         helper(nums, 0, k-1);
         helper(nums, k, n-1);
