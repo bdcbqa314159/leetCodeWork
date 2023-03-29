a7fdc851c50c82857a3409671da12c66cc6a4f59
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-29 21:26:01 +0100

    Time: 8 ms (51.25%), Space: 10.8 MB (19.05%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -6,8 +6,7 @@ public:
         for (int n: nums){
             z.insert(n);
             
-            if (z.size() > 3) z.erase(z.begin());
-            
+            if (z.size()>3) z.erase(z.begin());
         }
         
         return z.size() == 3 ? *z.begin(): *z.rbegin();
