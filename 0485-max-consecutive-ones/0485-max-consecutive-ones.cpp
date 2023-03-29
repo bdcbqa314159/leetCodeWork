d433ba8b86cfe49dc66631adc90b5e951fc51ff7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 00:04:11 +0100

    Time: 46 ms (30.19%), Space: 36.3 MB (24.30%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,15 +4,17 @@ public:
         int i = 0, curr = 0, ans = 0, n = nums.size();
         
         while (i<n){
+            
             if (nums[i] == 1){
                 curr++;
                 ans = max(ans, curr);
-            }        
+            }
             
             else curr = 0;
             i++;
         }
-    
+        
+        
         return ans;
     }
 };
