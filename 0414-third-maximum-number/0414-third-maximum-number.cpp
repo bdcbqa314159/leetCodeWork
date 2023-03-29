50c9dcea80c8ecf1ef1b3193524e3ed295bd46e4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-29 21:33:26 +0100

    Time: 16 ms (10.70%), Space: 10.8 MB (19.05%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         set<int> z;
-        
         for (int n: nums){
             z.insert(n);
+            
             if (z.size() > 3) z.erase(z.begin());
         }
         
