fd2f5069b886c98ae7724e4010a98baa3389ebf8
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-29 21:02:43 +0100

    Time: 0 ms (100.00%), Space: 6.9 MB (23.16%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         vector<int> count(26,0);
-        vector<bool> visited(26, false);
+        vector<bool> visited(26,false);
         stack<char> st;
         
-        for (char c: s)
-            count[c-'a']++;
-        
+        for (char c: s) count[c-'a']++;
         for (char c: s){
             int index = c-'a';
+            
             count[index]--;
             
             if (visited[index]) continue;
             
-            while (!st.empty() && st.top() > c && count[st.top()-'a']>0){
-                visited[st.top() - 'a'] = false;
+            while (!st.empty() && st.top()>c && count[st.top()-'a']>0){
+                visited[st.top()-'a'] = false;
                 st.pop();
             }
             
             st.push(c);
             visited[index] = true;
+            
         }
         
         string res;
+        
         while (!st.empty()){
             res = st.top()+res;
             st.pop();
