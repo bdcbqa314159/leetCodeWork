4f97cfc079dc77a1057612af9859d9b33077eac0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-29 21:17:09 +0100

    Time: 33 ms (9.28%), Space: 23.3 MB (51.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l = 0, r = s.size()-1;
-        while (l<r){
-            swap(s[l], s[r]); l++; r--;
-        }
-        return;
+        reverse(s.begin(), s.end());
     }
 };
