10c464c802926dff5f6b50a20e1d0ee296eebb8d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 11:52:09 +0100

    Time: 7 ms (60.73%), Space: 9.9 MB (14.58%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -6,13 +6,16 @@ public:
         while (n){
             n/=10;
             ans++;
-        }return ans;
+        }
+        return ans;
     }
     
     int findNumbers(vector<int>& nums) {
         int count = 0;
-        for (auto n: nums)
+        for (auto n: nums){
             if (helper(n)%2 == 0) count++;
+        }
+        
         return count;
     }
 };
