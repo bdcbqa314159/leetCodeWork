b50dd3fba0f45fb42953450b3359b03560fbd0b0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 11:34:13 +0100

    Time: 106 ms (11.92%), Space: 9.7 MB (84.38%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -4,11 +4,11 @@ public:
         int n = arr.size();
         for (int i = 0; i<n; i++){
             if (arr[i] == 0){
-                for (int j = n-1; j>i; j--){
+                for (int j = n-1; j>i; j--)
                     arr[j] = arr[j-1];
-                }i++;
+                i++;
             }
-        }
-        return;
+            
+        }return;
     }
 };
