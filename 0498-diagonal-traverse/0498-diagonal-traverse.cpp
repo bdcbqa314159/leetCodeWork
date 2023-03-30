dee503b1626c7e0848f0571541b89db16fb590f9
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 09:38:44 +0100

    Time: 36 ms (46.89%), Space: 17.8 MB (92.08%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -4,22 +4,31 @@ public:
         if (mat.size() == 0) return {};
         if (mat[0].size() == 0) return {};
         
-        int m = mat.size(), n = mat[0].size();
+//         int m = mat.size(), n = mat[0].size();
+//         int total = m*n;
         
+//         vector<int> ans(total);
+        
+//         int i = 0, j = 0;
+//         bool up = true;
+        
+        int m = mat.size(), n = mat[0].size();
         int total = m*n;
         
         vector<int> ans(total);
-        
         int i = 0, j = 0;
         bool up = true;
         
-        for (int k = 0; k< total; k++){
+        
+        for (int k = 0; k<total; k++){
             ans[k] = mat[i][j];
+            
             if (up && (i == 0 || j == n-1)){
-                if (j == n-1) i++;
+                if (j==n-1)i++;
                 else j++;
                 up = !up;
             }
+
             
             else if (!up && (j == 0 || i == m-1)){
                 if (i == m-1) j++;
@@ -29,8 +38,11 @@ public:
             
             else{
                 if (up){i--; j++;}
-                else {i++; j--;}
+                else{i++; j--;}
             }
-        }return ans;
+        }
+ 
+        
+        return ans;
     }
 };
