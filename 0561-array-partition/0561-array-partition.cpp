a23c727a82ddeddd4b18cda4196a532daf47b0bb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 10:23:28 +0100

    Time: 50 ms (91.69%), Space: 28.3 MB (57.60%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -6,7 +6,7 @@ public:
         sort(nums.begin(), nums.end());
         
         for (int i = 0; i<n; i++){
-            if (i%2 == 0) sum+=nums[i];
+            if (i % 2 == 0) sum += nums[i];
         }
         
         return sum;
