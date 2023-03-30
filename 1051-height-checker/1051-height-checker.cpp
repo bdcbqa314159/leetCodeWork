bea10658c41a3cd366143954148b23d7a1371ab1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 11:28:17 +0100

    Time: 0 ms (100.00%), Space: 8.3 MB (30.22%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -3,13 +3,11 @@ public:
     int heightChecker(vector<int>& heights) {
         int n = heights.size(), count = 0;
         vector<int> a(heights);
-        
         sort(a.begin(), a.end());
         
         for (int i = 0; i<n; i++){
             if (a[i]!=heights[i]) count++;
         }
-        
         return count;
     }
 };
