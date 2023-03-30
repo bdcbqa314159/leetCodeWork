85a4146a1b7c53eeed3095c6b494aabb77b33c03
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 11:13:10 +0100

    Time: 33 ms (42.62%), Space: 22.4 MB (57.54%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -5,8 +5,7 @@ public:
         while (i<n && i+1<n && arr[i]<arr[i+1]) i++;
         if (i == 0 || i>=n-1) return false;
         while (i<n && i+1<n){
-            if (arr[i]<=arr[i+1]) return false;
-            i++;
+            if (arr[i]<=arr[i+1]) return false;i++;
         }
         return true;
     }
