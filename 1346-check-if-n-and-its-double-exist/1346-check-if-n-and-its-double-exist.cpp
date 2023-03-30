b6649e455c865e6060c8c00c32a46ead9bf38e90
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 12:28:49 +0100

    Time: 8 ms (66.47%), Space: 10.5 MB (23.34%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     
-    bool helper(int i, unordered_map<int,int>& u){
+    bool helper(int i, unordered_map<int,int>&u){
         return u.find(i)!=u.end();
     }
     
     bool checkIfExist(vector<int>& arr) {
         unordered_map<int,int> umap;
-        
-        for (int n: arr){
+        for (int n : arr){
             if (n == 0){
                 if (helper(n, umap)) return true;
                 umap[n]++;
             }
-            
             else{
                 if (n%2 == 0){
-                    if( helper(n/2, umap) )return true;
+                    if (helper(n/2, umap)) return true;
                     umap[n]++;
                 }
-                
                 if (helper(2*n, umap)) return true;
                 umap[n]++;
             }
