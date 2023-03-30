53902e7f880b1d6769a7d4668cfb5ce5628f5f28
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-03-30 08:06:14 +0100

    Time: 37 ms (81.15%), Space: 36.3 MB (61.07%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.cpp
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.cpp
@@ -5,9 +5,9 @@ public:
         if (n == 0) return 0;
         
         int j = 0, zeros = 0;
-        for (int i = 0; i<n; i++){
+        
+        for (int i =0; i<n; i++){
             if (nums[i] == 0) zeros++;
-            
             while (j<=i && zeros > 1){
                 if (nums[j] == 0) zeros--;
                 j++;
