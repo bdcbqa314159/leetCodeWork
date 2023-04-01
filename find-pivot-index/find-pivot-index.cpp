0aa9357058e9d1287e2a796945f2d22ef75ac1f3
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-01 23:55:00 +0100

    Time: 18 ms, Memory: 31.1 MB - LeetHub

--- find-pivot-index/find-pivot-index.cpp
+++ find-pivot-index/find-pivot-index.cpp
@@ -3,12 +3,11 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size(), left = 0;
         int total = accumulate(nums.begin(), nums.end(), 0);
+        
         for (int i = 0; i<n; i++){
             if (left == total - left - nums[i]) return i;
             left += nums[i];
         }
-        
         return -1;
-        
     }
 };
