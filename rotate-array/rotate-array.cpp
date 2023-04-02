9f0818d3bbd3de0c561e38c3bb3161627540fe6e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 15:41:34 +0100

    Time: 26 ms, Memory: 24.9 MB - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -3,13 +3,10 @@ public:
     
     void helper(vector<int>& nums, int l, int r){
         while(l<=r){
-            swap(nums[l], nums[r]);
-            l++; r--;
-            
+            swap(nums[l], nums[r]); l++; r--;
         }
         return;
     }
-    
     
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
@@ -18,6 +15,7 @@ public:
         helper(nums, 0, n-1);
         helper(nums, 0, k-1);
         helper(nums, k, n-1);
+        
         return;
     }
 };
