818bbc1c2e0342d8308c7b706d1713abb0e481f5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 01:28:11 +0100

    Time: 0 ms, Memory: 6.8 MB - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         if (matrix.size() == 0) return {};
+        
         int i = 0, j = 0;
         int h = matrix.size(), w = matrix[0].size();
         
-        int step = 1;
-        
         vector<int> result(h*w);
-        
+        int step = 1;
         int index = 0;
         
-        while(h>0 && w>0){
-            
+        while (h>0 && w>0){
             for (int k = 0; k<w; k++){
                 result[index] = matrix[i][j];
-                index++;
                 j+=step;
+                index++;
             }
             
             j-=step;
@@ -25,16 +23,15 @@ public:
             
             for (int k = 0; k<h; k++){
                 result[index] = matrix[i][j];
-                index++;
                 i+=step;
+                index++;
             }
+            
             i-=step;
             step*=-1;
             j+=step;
             w--;
-            
         }
-        
         return result;
     }
 };
