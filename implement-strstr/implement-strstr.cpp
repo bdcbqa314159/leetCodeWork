4baacf5369dca0fd445ae0e431e36068a2d2a433
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 13:51:27 +0100

    Time: 5 ms, Memory: 6.3 MB - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         if (needle.size() == 0) return 0;
-        int n = haystack.size(), m = needle.size();
-        
+        int n=haystack.size(), m = needle.size();
         if (n<m) return -1;
-        for (int i = 0; i<=n-m; i++){
-            if (haystack.substr(i, m) == needle) return i;
-        }
+        for (int i = 0; i<=n-m; i++)
+            if (haystack.substr(i,m) == needle) return i;
         return -1;
     }
 };
