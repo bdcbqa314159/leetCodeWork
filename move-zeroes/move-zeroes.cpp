d60f6ea983c084e271849c358756976f4519182d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 16:16:52 +0100

    Time: 31 ms, Memory: 19.2 MB - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0, n = nums.size();
-        for (int i = 0;i<n; i++){
+        int position = 0, n = nums.size();
+        
+        for (int i = 0; i<n; i++){
             if (nums[i]!=0){
-                nums[j] = nums[i];
-                j++;
+                nums[position] = nums[i];
+                position++;
             }
         }
-        for (int i = j; i<n; i++) nums[i] = 0;
+        
+        for (int i = position; i<n; i++)
+            nums[i] = 0;
+        return;
     }
 };
