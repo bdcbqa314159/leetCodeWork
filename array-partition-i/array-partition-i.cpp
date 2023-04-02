60616470a3657463ce82b621fe6102c8e96837d0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 14:06:08 +0100

    Time: 59 ms, Memory: 28.2 MB - LeetHub

--- array-partition-i/array-partition-i.cpp
+++ array-partition-i/array-partition-i.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        int sum = 0, n = nums.size();
+        int n = nums.size(), sum = 0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i<n; i+=2){
+        
+        for (int i = 0; i<n; i+=2)
             sum+=nums[i];
-        }
+        
         return sum;
     }
 };
