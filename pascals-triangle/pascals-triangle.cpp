577335d3a844dd4f9200d87fa03aa9d18b333d55
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-02 01:36:55 +0100

    Time: 0 ms, Memory: 6.7 MB - LeetHub

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -4,11 +4,10 @@ public:
         vector<vector<int>> r(numRows);
         
         for (int i = 0; i<numRows; i++){
-            r[i].resize(i+1);
-            r[i][0] = r[i][i] = 1;
+            r[i].resize(i+1, 1);
             
             for (int j = 1; j<i; j++){
-                r[i][j] = r[i-1][j-1]+r[i-1][j];
+                r[i][j] = r[i-1][j]+r[i-1][j-1];
             }
         }
         return r;
