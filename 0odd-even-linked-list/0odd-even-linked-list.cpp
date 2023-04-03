38421e2de034e70785ef7a7af6198c3421e9594e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-03 21:34:13 +0100

    Time: 7 ms, Memory: 10.5 MB - LeetHub

--- 0odd-even-linked-list/0odd-even-linked-list.cpp
+++ 0odd-even-linked-list/0odd-even-linked-list.cpp
@@ -12,17 +12,18 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if (!head) return 0;
-        
         ListNode *odd = head, *even = head->next, *evenHead = even;
         
-        while(even && even->next){
+        while (even && even->next){
             odd->next = even->next;
             odd = odd->next;
+            
             even->next = odd->next;
             even = even->next;
         }
         
         odd->next = evenHead;
+        
         return head;
     }
 };
