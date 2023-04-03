a619ce731f8a0980706bac1a1d33c225a82d50d8
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-03 21:02:22 +0100

    Time: 50 ms, Memory: 14.6 MB - LeetHub

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return 0;
+        
+        ListNode *nodeA = headA, *nodeB = headB;
+        
+        while (nodeA!=nodeB){
+            if (!nodeA) nodeA = headB;
+            else nodeA = nodeA->next;
+            
+            if (!nodeB) nodeB = headA;
+            else nodeB = nodeB->next;
+        }
+        
+        return nodeA;
+    }
+};
