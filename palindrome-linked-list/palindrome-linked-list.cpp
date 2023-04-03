a863f305484efae59eb9cb1c2f3738a908d34c2a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-03 21:56:59 +0100

    Time: 264 ms, Memory: 122.9 MB - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* frontPointer;
+    bool helper(ListNode* current){
+        if (current){
+            
+            if (!helper(current->next)) return false;
+            if (current->val != frontPointer->val) return false;
+            frontPointer = frontPointer->next;
+            
+        }
+        return true;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        frontPointer = head;
+        
+        return helper(head);
+    }
+};
