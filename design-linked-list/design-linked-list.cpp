6fef4565b3570122e521c21e497daa5e52bce0f5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-07 23:34:00 +0100

    Time: 36 ms, Memory: 19.6 MB - LeetHub

--- design-linked-list/design-linked-list.cpp
+++ design-linked-list/design-linked-list.cpp
@@ -1,54 +1,50 @@
 class Node{
-public:
+    
+    public:
     int val{0};
     Node* next{nullptr};
-    
     Node(){}
-    
-    Node(int val):val(val), next(nullptr){
-        
-    }
-    
+    Node(int val):val(val), next(nullptr){}
 };
+
+
 
 class MyLinkedList {
 public:
     
-    int size = 0;
+    int size{0};
     Node* head = new Node(0);
-    
     
     MyLinkedList() {
         
     }
     
-        int get(int index) {
+    int get(int index) {
         if (index<0 || index>=size) return -1;
-        Node *temp = head->next;
+        Node* temp = head->next;
         for (int i = 0; i<index; i++) temp = temp->next;
         return temp->val;
     }
-    
     
     void addAtHead(int val) {
         Node* temp = head->next;
         head->next = new Node(val);
         head->next->next = temp;
         size++;
+        return;
     }
-    
-
     
     void addAtTail(int val) {
         Node* temp = head;
         while (temp->next) temp = temp->next;
         temp->next = new Node(val);
         size++;
+        return;
     }
     
     void addAtIndex(int index, int val) {
         if (index<0 || index>size) return;
-        Node *temp = head;
+        Node* temp = head;
         for (int i = 0; i<index; i++) temp = temp->next;
         Node* temp1 = temp->next;
         temp->next = new Node(val);
@@ -59,75 +55,16 @@ public:
     }
     
     void deleteAtIndex(int index) {
-        if (index>=size) return;
+        if (index<0 || index>=size) return;
         Node* temp = head;
-        for (int i= 0; i<index; i++) temp = temp->next;
+        for (int i = 0; i<index; i++) temp = temp->next;
         Node* temp1 = temp->next;
         temp->next = temp1->next;
         temp1->next = nullptr;
         size--;
+        return;
     }
 };
-
-
-// class MyLinkedList {
-// public:
-    
-//     int size{0};
-//     Node* head = new Node(0);
-    
-    
-//     MyLinkedList() {
-        
-//     }
-    
-    // int get(int index) {
-    //     if (index<0 || index>=size) return -1;
-    //     Node *temp = head->next;
-    //     for (int i = 0; i<index; i++) temp = temp->next;
-    //     return temp->val;
-    // }
-    
-//     void addAtHead(int val) {
-//         Node* temp = head->next;
-//         head->next = new Node(val);
-//         head->next->next = temp;
-//         size++;
-//         return;
-//     }
-    
-    // void addAtTail(int val) {
-    //     Node* temp = head;
-    //     while (temp->next) temp = temp->next;
-    //     temp->next = new Node(val);
-    //     size++;
-    //     return;
-    // }
-    
-//     void addAtIndex(int index, int val) {
-//         if (index<0 || index>=size) return;
-//         Node *temp = head;
-//         for (int i = 0; i<index; i++) temp = temp->next;
-//         Node* temp1 = temp->next;
-//         temp->next = new Node(val);
-//         temp->next->next = temp1;
-//         size++;
-//         return;
-        
-//     }
-    
-//     void deleteAtIndex(int index) {
-//         if (index<0 || index > size) return;
-//         Node* temp = head;
-//         for (int i = 0; i<index; i++) temp = temp->next;
-//         Node* temp1 = temp->next;
-//         temp->next = temp1->next;
-//         temp1->next = nullptr;
-//         size--;
-//         return;
-        
-//     }
-// };
 
 /**
  * Your MyLinkedList object will be instantiated and called as such:
