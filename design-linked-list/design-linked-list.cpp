f969955196fe42a76b87a03eef2f7bcb68ac9cb1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-08 18:50:00 +0100

    Time: 44 ms, Memory: 20 MB - LeetHub

--- design-linked-list/design-linked-list.cpp
+++ design-linked-list/design-linked-list.cpp
@@ -1,45 +1,48 @@
 class Node{
+    
     public:
     int val;
-    Node *prev, *next;
-    Node(int val):val(val), prev(nullptr), next(nullptr){}
+    Node *next, *prev;
+    Node(int val): val(val), next(nullptr), prev(nullptr){}
 };
 
 
 class MyLinkedList {
 public:
-    
-    Node* head = nullptr;
+    Node *head = nullptr;
     
     MyLinkedList() {
         
     }
     
     Node *getNode(int index){
-        Node *temp = head;
-        for (int i = 0; i<index && temp; ++i)
-            temp = temp->next;
-        return temp;
+        Node *curr = head;
+        for (int i = 0; i<index && curr; i++){
+            curr = curr->next;
+        }
+        return curr;
     }
     
     Node *getTail(){
-        Node *temp = head;
-        while (temp && temp->next)
-            temp = temp->next;
-        return temp;
+        Node *curr = head;
+        while (curr && curr->next){
+            curr = curr->next;
+        }
+        return curr;
     }
     
     int get(int index) {
-        Node *temp = getNode(index);
-        return temp == nullptr? -1: temp->val;
+        Node *node = getNode(index);
+        return node == nullptr ? -1:node->val;
     }
     
     void addAtHead(int val) {
-        Node *temp = new Node(val);
-        temp->next = head;
-        if (head)
-            head->prev = temp;
-        head = temp;
+        Node *curr = new Node(val);
+        
+        curr->next = head;
+        if (head) head->prev = curr;
+        
+        head = curr;
         return;
     }
     
@@ -68,13 +71,14 @@ public:
         
         Node *curr = new Node(val);
         Node *next = prev->next;
+        
         curr->prev = prev;
         curr->next = next;
+        
         prev->next = curr;
         
         if (next) next->prev = curr;
         return;
-        
     }
     
     void deleteAtIndex(int index) {
