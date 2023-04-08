8703d9996b210d580eacf2cce62c65a02e90a042
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-08 22:50:55 +0100

    Time: 6 ms, Memory: 8.2 MB - LeetHub

--- insert-into-a-cyclic-sorted-list/insert-into-a-cyclic-sorted-list.cpp
+++ insert-into-a-cyclic-sorted-list/insert-into-a-cyclic-sorted-list.cpp
@@ -23,19 +23,18 @@ class Solution {
 public:
     Node* insert(Node* head, int insertVal) {
         if (!head){
-            Node *newNode = new Node(insertVal, nullptr);
+            Node *newNode = new Node(insertVal, 0);
             newNode->next = newNode;
             return newNode;
         }
         
-        Node *prev = head;
-        Node *curr = head->next;
+        Node *prev = head, *curr = head->next;
         bool toInsert = false;
         
         do{
+            
             if (prev->val <= insertVal && insertVal <= curr->val)
                 toInsert = true;
-            
             else if(prev->val > curr->val){
                 if (insertVal >= prev->val || insertVal <=curr->val)
                     toInsert = true;
@@ -49,8 +48,8 @@ public:
             prev = curr;
             curr = curr->next;
             
-            
-        }while (prev!=head);
+        }while(prev!=head);
+        
         prev->next = new Node(insertVal, curr);
         return head;
     }
