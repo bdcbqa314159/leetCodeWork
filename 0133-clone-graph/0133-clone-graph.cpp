a5b5a9d9ab67578760a2b4173f6375255f78e5d4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-08 14:26:17 +0100

    Time: 3 ms (95.03%), Space: 8.8 MB (65.38%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    Node* helper(Node* node, unordered_map<int, Node*> &visited){
+        Node* newNode = new Node(node->val);
+        visited.insert({newNode->val, newNode});
+        
+        for (Node* n: node->neighbors){
+            auto it = visited.find(n->val);
+            if (it==visited.end()){
+                Node* clone = helper(n, visited);
+                newNode->neighbors.push_back(clone);
+            }
+            else
+                newNode->neighbors.push_back(it->second);
+        }
+        
+        return newNode;
+    }
+    
+    Node* cloneGraph(Node* node) {
+        if (!node) return 0;
+        unordered_map<int,Node*> visited;
+        return helper(node, visited);
+    }
+};
