cca4ed544b741790e2838f8b1fae3a73a2131a8e
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-08 16:50:49 +0100

    Time: 7 ms, Memory: 10.7 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,16 +13,13 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *ans = head, *first = head, *second = head;
         for (int i = 0; i<n; i++) first = first->next;
-        if (!first){
-            ans = head->next;
-            return ans;
-        }
+        
+        if (!first) return head->next;
         
         while (first->next){
             first = first->next;
             second = second->next;
         }
-        
         second->next = second->next->next;
         return ans;
     }
