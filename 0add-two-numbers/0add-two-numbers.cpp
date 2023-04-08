1c63a33d9ab53fd21d210ac75b9fb46df14c882f
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-08 19:30:40 +0100

    Time: 43 ms, Memory: 71.6 MB - LeetHub

--- 0add-two-numbers/0add-two-numbers.cpp
+++ 0add-two-numbers/0add-two-numbers.cpp
@@ -13,21 +13,23 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *ans = new ListNode(0), *temp = ans;
         int carry = 0;
-        
         while (l1 || l2 || carry){
             int v1 = 0, v2 = 0;
             if (l1){
                 v1 = l1->val;
                 l1 = l1->next;
             }
+            
             if (l2){
                 v2 = l2->val;
                 l2 = l2->next;
             }
             
             int sum = v1+v2+carry;
+            
             temp->next = new ListNode(sum%10);
             temp = temp->next;
+            
             carry = sum/10;
         }
         return ans->next;
