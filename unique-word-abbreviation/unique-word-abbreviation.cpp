27ce74f7059e35cc7448c79b6b1824d76ce95578
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-09 19:01:58 +0100

    Time: 167 ms, Memory: 62.7 MB - LeetHub

---
+++ unique-word-abbreviation/unique-word-abbreviation.cpp
@@ -0,0 +1,26 @@
+class ValidWordAbbr {
+public:
+    
+    unordered_map<string, unordered_set<string>> mp;
+    
+    ValidWordAbbr(vector<string>& dictionary) {
+        for (string &d : dictionary){
+            int n = d.size();
+            string abbr = d[0]+to_string(n)+d[n-1];
+            mp[abbr].insert(d);
+        }
+        return;
+    }
+    
+    bool isUnique(string word) {
+        int n = word.size();
+        string abbr = word[0]+to_string(n)+word[n-1];
+        return mp[abbr].count(word) == mp[abbr].size();
+    }
+};
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr* obj = new ValidWordAbbr(dictionary);
+ * bool param_1 = obj->isUnique(word);
+ */
