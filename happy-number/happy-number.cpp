a95c34ae59c0264ca10d437317651e41dc4e111a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-09 17:16:19 +0100

    Time: 4 ms, Memory: 6 MB - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int sumSquare(int n){
+        int res = 0;
+        while(n){
+            res+=(n%10)*(n%10);
+            n/=10;
+        }
+        return res;
+    }
+    
+    
+    bool isHappy(int n) {
+        vector<int> u = {1,2,4,16,20,37,42,58,89,145};
+        int s = 0;
+        while(find(u.begin(), u.end(), s) == u.end()){
+            s = sumSquare(n);
+            n = s;
+        }
+        return s == 1;
+    }
+};
