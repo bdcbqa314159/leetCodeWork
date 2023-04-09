26f057a7c0453419695728b1c37b66e4f1dee399
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-09 18:47:03 +0100

    Time: 177 ms, Memory: 27.9 MB - LeetHub

---
+++ 0two-sum-iii-data-structure-design/0two-sum-iii-data-structure-design.cpp
@@ -0,0 +1,31 @@
+class TwoSum {
+public:
+    unordered_multiset<int> nums;
+    
+    TwoSum() {
+        
+    }
+    
+    void add(int number) {
+        nums.insert(number);
+    }
+    
+    bool find(int value) {
+        for (unsigned int i : nums){
+            unsigned int diff = value-i;
+            int count = i==diff? 1:0;
+            
+            if (nums.count(value-i) >count)
+                return true;
+            
+        }
+        return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
