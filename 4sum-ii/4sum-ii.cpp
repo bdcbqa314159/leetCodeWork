8ce7f561845bb0e4b2e56b446818a440d7e9b075
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-09 18:50:13 +0100

    Time: 321 ms, Memory: 24.4 MB - LeetHub

---
+++ 4sum-ii/4sum-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int ans = 0;
+        unordered_map<int,int> umap;
+        
+        int l1 = nums1.size();
+        int l2 = nums2.size();
+        int l3 = nums3.size();
+        int l4 = nums4.size();
+        
+        for (const auto &x: nums1){
+            for (const auto &y: nums2){
+                if (umap.find(x+y) == umap.end())
+                    umap[x+y] = 1;
+                else
+                    umap[x+y]++;
+            }
+        }
+        
+        for (const auto &x: nums3){
+            for (const auto &y: nums4){
+                int target = -(x+y);
+                if (umap.find(target) != umap.end())
+                    ans+=umap[target];
+            }
+        }
+        return ans;
+        
+    }
+};
