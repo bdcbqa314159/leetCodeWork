aa3e053d52891d8f6fae6f53132e1f85932d7632
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-13 00:22:38 +0100

    Time: 178 ms, Memory: 52.1 MB - LeetHub

---
+++ design-hashmap/design-hashmap.cpp
@@ -0,0 +1,53 @@
+class MyHashMap {
+public:
+    
+    int size = 10;
+    vector<list<pair<int,int>>> mp = vector<list<pair<int,int>>>(size);
+    
+    int hash(int key){
+        return key%size;
+    }
+    
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        int i = hash(key);
+        for (auto it = mp[i].begin(); it!=mp[i].end(); it++){
+            if (it->first == key){
+                it->second = value;
+                return;
+            } 
+        }
+        mp[i].push_back({key,value});
+    }
+    
+    int get(int key) {
+        int i = hash(key);
+        for (auto it = mp[i].begin(); it!=mp[i].end(); it++){
+            if (it->first == key)
+                return it->second;
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        int i = hash(key);
+        for (auto it = mp[i].begin(); it!=mp[i].end(); it++){
+            if (it->first == key){
+                mp[i].erase(it);
+                return;
+            }  
+        }
+        return;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
