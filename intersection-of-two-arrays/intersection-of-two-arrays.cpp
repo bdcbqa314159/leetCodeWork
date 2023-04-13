7fa0d4f9e5c1daf99d0e673fbc4cdb1b37d0e75c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-13 08:03:39 +0100

    Time: 0 ms, Memory: 10.8 MB - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> a, b;
+        set<int> a,b;
         vector<int> v;
         for (auto n: nums1)
             a.insert(n);
@@ -9,7 +9,7 @@ public:
             b.insert(n);
         
         auto it = a.begin();
-        for (it ; it!=a.end(); it++){
+        for (it; it!=a.end(); it++){
             if (b.find(*it) != b.end())
                 v.push_back(*it);
         }
