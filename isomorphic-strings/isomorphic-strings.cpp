1304465297b769af87e53644b54cf411c6bf9bbb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-13 08:43:48 +0100

    Time: 7 ms, Memory: 7.1 MB - LeetHub

--- isomorphic-strings/isomorphic-strings.cpp
+++ isomorphic-strings/isomorphic-strings.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         map<char,char> m;
-        vector<bool> p(256,false);
+        vector<bool> visited(256,false);
         
-        int len1 = s.size();
-        for (int i = 0; i<len1; i++){
+        int n = s.size();
+        for (int i = 0; i<n; i++){
             char c1 = s[i];
             char c2 = t[i];
             
-            if (m.count(c1) == 0){
-                if (p[c2]) return false;
+            if (m.find(c1) == m.end()){
+                
+                if (visited[c2]) return false;
+                
                 m[c1] = c2;
-                p[c2] = true;
+                visited[c2] = true;
+                
             }
             
             else{
-                if (m[c1] != c2)
+                if (m[c1]!=c2)
                     return false;
             }
         }
