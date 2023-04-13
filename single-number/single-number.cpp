d541f7a8db6ec79826bb7aae4ee957e8d467441a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-13 01:34:39 +0100

    Time: 15 ms, Memory: 16.9 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for (int i = 0; i<nums.size(); i++)
-            result ^= nums[i];
+        for (int n: nums) result ^= n;
         return result;
     }
 };
