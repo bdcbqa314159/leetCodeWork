198ee86786e967842d72db65b77053a06a118ee1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-13 09:28:31 +0100

    Time: 99 ms, Memory: 36.5 MB - LeetHub

--- minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         int n = list1.size(), m = list2.size();
-        map<string,int> mapping;
-        vector<string> result;
+        map<string, int> map_;
         int minSum = INT_MAX;
+        vector<string> result;
         
         for (int i = 0; i<n; i++)
-            mapping[list1[i]] = i;
+            map_[list1[i]] = i;
         
         for (int j = 0; j<m; j++){
-            auto it = mapping.find(list2[j]);
+            auto it = map_.find(list2[j]);
+            if (it == map_.end()) continue;
             
-            if (it == mapping.end()) continue;
-            
-            int idx = it->second+j;
-            
-            if (idx == minSum) result.push_back(list2[j]);
-            if (idx<minSum){
-                minSum = idx;
+            int index = it->second+j;
+            if (index == minSum) result.push_back(list2[j]);
+            if (index<minSum){
+                minSum = index;
                 result.clear();
                 result.push_back(list2[j]);
             }
-        }
+         }
         return result;
     }
 };
