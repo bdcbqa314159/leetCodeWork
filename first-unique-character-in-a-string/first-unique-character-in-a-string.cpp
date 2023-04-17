da964103404334cb8605616209b3027f00d5397d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-17 17:19:48 +0100

    Time: 27 ms, Memory: 10.6 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.size();
-        vector<int> dict(26);
+        vector<int> ans(26);
         
         for (auto x: s)
-            dict[x-'a']++;
+            ans[x-'a']++;
         
         for (int i = 0; i<n; i++){
-            if (dict[s[i]-'a'] == 1)
-                return i;
+            if (ans[s[i]-'a'] == 1) return i;
         }
         return -1;
     }
