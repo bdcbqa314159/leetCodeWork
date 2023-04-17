24e295aa6ee88a2cb6ba7957e92ca02b42e2d3f1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-17 17:45:43 +0100

    Time: 183 ms, Memory: 77.4 MB - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.cpp
+++ contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
         int n = nums.size(), x = 0;
         unordered_map<int,int> m;
+        
         for (int i = 0; i<n; i++){
+            
             x = nums[i];
             if (m.find(x) != m.end() && i-m[x]<=k)
                 return true;
             m[x] = i;
         }
+        
         return false;
     }
 };
