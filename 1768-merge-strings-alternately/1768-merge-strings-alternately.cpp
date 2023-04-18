d8eea56b926554993062a8140416decbd9016e06
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-04-18 23:53:03 +0100

    Time: 4 ms (38.94%), Space: 6.4 MB (19.35%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string s = "";
-        int m = word1.size(), n = word2.size(), i=0, j=0;
-        
-        while (i<m && j<n){
-            s+=word1[i];
-            s+=word2[j];
-            i++;
-            j++;
+        int m = word1.size();
+        int n = word2.size();
+        string result = "";
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.push_back(word1[i++]);
+            }
+            if (j < n) {
+                result.push_back(word2[j++]);
+            }
         }
-        
-        while (j<n){
-            s+=word2[j];j++;
-        }
-        
-        
-        while (i<m){
-            s+=word1[i];i++;
-        }
-        return s;
+
+        return result;
     }
 };
