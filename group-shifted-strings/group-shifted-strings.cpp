e29b398aff8023ad07a34657dc8208202d6f42d5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-04 14:04:14 +0100

    Time: 0 ms, Memory: 8 MB - LeetHub

--- group-shifted-strings/group-shifted-strings.cpp
+++ group-shifted-strings/group-shifted-strings.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     
     string shift(string &s){
+        
         string t;
         int n = s.size();
         
         for (int i = 1; i<n; i++){
             int diff = s[i]-s[i-1];
             if (diff<0) diff+=26;
+            
             t+='a'+diff+',';
         }
+        
         return t;
     }
     
+    
     vector<vector<string>> groupStrings(vector<string>& strings) {
+        vector<vector<string>> result;
         unordered_map<string, vector<string>> umap;
-        vector<vector<string>> groups;
         
-        for (auto x: strings){
-            umap[shift(x)].push_back(x);
+        for (string s: strings)
+            umap[shift(s)].push_back(s);
+        
+        for (auto item: umap){
+            result.push_back(item.second);
         }
-        
-        for (auto m: umap){
-            vector<string> group = m.second;
-            
-            //sort(group.begin(), group.end());
-            
-            groups.push_back(group);
-        }
-        
-        return groups;
-        
+        return result;
     }
 };
