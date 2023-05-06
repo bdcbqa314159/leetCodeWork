0a6a2968de4b08307155df1a941dce5f7a6d7a42
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-06 16:54:36 +0100

    Time: 0 ms, Memory: 6.4 MB - LeetHub

--- jewels-and-stones/jewels-and-stones.cpp
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -5,17 +5,15 @@ public:
         for (int i = 0; i<jewels.size(); i++)
             umap[jewels[i]] = 0;
         
-        for (int i = 0; i<stones.size(); i++){
-            if (umap.find(stones[i])!=umap.end())
+        for (int i = 0;i<stones.size(); i++){
+            if (umap.find(stones[i]) != umap.end())
                 umap[stones[i]]++;
         }
         
         int sum = 0;
         
-        unordered_map<char, int>::iterator it;
-        
-        for (it = umap.begin(); it!= umap.end(); it++)
-            sum+=it->second;
+        for (auto x: umap)
+            sum+=x.second;
         return sum;
     }
 };
