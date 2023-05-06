9d3e84131b735dd1a50e7a30f41735f64174277d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-06 17:09:29 +0100

    Time: 18 ms, Memory: 8.3 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size(), ans = 0;
+        
         if (n == 0) return ans;
         
         int l = 0, r = 0;
         
-        unordered_map<char,int> umap;
+        unordered_map<char, int> umap;
         
         while (l<n && r<n){
             char e = s[r];
@@ -17,10 +18,8 @@ public:
             ans = max(ans, r-l+1);
             
             umap[e] = r;
-            
             r++;
         }
-        
         return ans;
     }
 };
