3a1eb6859d999da6634b29ae7784a1f403b23414
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-06 18:38:19 +0100

    Time: 186 ms, Memory: 64.1 MB - LeetHub

--- unique-word-abbreviation/unique-word-abbreviation.cpp
+++ unique-word-abbreviation/unique-word-abbreviation.cpp
@@ -4,7 +4,7 @@ public:
     unordered_map<string, unordered_set<string>> mp;
     
     ValidWordAbbr(vector<string>& dictionary) {
-        for (string &d : dictionary){
+        for (string d: dictionary){
             int n = d.size();
             string abbr = d[0]+to_string(n)+d[n-1];
             mp[abbr].insert(d);
