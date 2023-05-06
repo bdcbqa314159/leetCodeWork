8accf4d1eab6d2e0d92b6d513dd0ae92edd46aeb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-06 18:18:47 +0100

    Time: 16 ms, Memory: 13.8 MB - LeetHub

--- 0top-k-frequent-elements/0top-k-frequent-elements.cpp
+++ 0top-k-frequent-elements/0top-k-frequent-elements.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     
     struct comp{
-        inline bool operator()(const pair<int,int> &l, const pair<int,int> &r){
+        inline bool operator()(const pair<int,int>&l, const pair<int,int> &r){
             return l.second>r.second;
         }
     };
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int,int> m;
+        
         vector<pair<int,int>> v;
         
         for (int x: nums) m[x]++;
         for (auto x: m)
-            v.push_back(make_pair(x.first,x.second));
+            v.push_back(make_pair(x.first, x.second));
         sort(v.begin(), v.end(), comp());
-        
-        vector<int> result;
-        
-        for (int i = 0; i<k; i++)
+        vector<int>result;
+        for (int i = 0; i<k;i++)
             result.push_back(v[i].first);
         return result;
     }
