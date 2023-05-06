573cb4c22abfc224d08c20440eb8de8cd84a5e89
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-06 16:59:05 +0100

    Time: 0 ms, Memory: 6.3 MB - LeetHub

--- jewels-and-stones/jewels-and-stones.cpp
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char, int> umap;
-        for (int i = 0; i<jewels.size(); i++)
-            umap[jewels[i]] = 0;
-        
-        for (int i = 0; i<stones.size(); i++){
-            if (umap.find(stones[i])!=umap.end())
-                umap[stones[i]]++;
+        unordered_map<char,int> umap;
+        for (auto x: jewels)
+            umap[x]=0;
+        for (auto x: stones){
+            if (umap.find(x)!=umap.end())
+                umap[x]++;
         }
         
         int sum = 0;
