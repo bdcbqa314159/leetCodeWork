1064763841f99579c7d362e1d57e17ec9362b0ae
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-14 23:42:46 +0100

    Time: 0 ms (100.00%), Space: 6.1 MB (56.11%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int helper(int n){
+        int res = 0;
+        while (n){
+            res+=(n%10)*(n%10);
+            n/=10;
+        }
+        return res;
+    }
+    
+    bool isHappy(int n) {
+        vector<int> u = {1,2,4,16,20,37,42,58,89,145};
+        int s = 0;
+        while (find(u.begin(), u.end(), s) == u.end()){
+            s = helper(n);
+            n = s;
+        }
+        
+        return 1==s;
+    }
+};
+
+//vector<int> u = {1,2,4,16,20,37,42,58,89,145};
