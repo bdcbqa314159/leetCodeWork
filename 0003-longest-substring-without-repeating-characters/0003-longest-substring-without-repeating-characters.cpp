5bfcdf07561ae663fd6292938e23decb45747ce1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-14 19:07:12 +0100

    Time: 14 ms (75.37%), Space: 8.4 MB (64.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         if (n == 0) return ans;
         
         int l = 0, r = 0;
-        unordered_map<char, int> umap;
+        unordered_map<char,int> umap;
         
         while (l<n && r<n){
             char e = s[r];
