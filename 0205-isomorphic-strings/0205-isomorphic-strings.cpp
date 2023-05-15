0d47e689dc5307c6185944e7fbda073e903e5637
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-15 23:01:42 +0100

    Time: 10 ms (39.63%), Space: 7 MB (71.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,6 +4,7 @@ public:
         int n = s.size();
         vector<bool> visited(256,false);
         map<char,char> m;
+        
         for (int i = 0; i<n; i++){
             char c1 = s[i], c2 = t[i];
             if (m.find(c1) == m.end()){
