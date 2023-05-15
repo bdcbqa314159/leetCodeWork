beaa344f06bb0a27b00eba354ffbf2f99d76f011
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-15 23:14:17 +0100

    Time: 188 ms (58.09%), Space: 77.1 MB (78.35%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -5,10 +5,9 @@ public:
         unordered_map<int,int> m;
         for (int i = 0; i<n; i++){
             int x = nums[i];
-            if (m.find(x)!=m.end() && i-m[x]<=k)
+            if (m.find(x) != m.end() && i-m[x]<=k)
                 return true;
             m[x] = i;
-        }
-        return false;
+        }return false;
     }
 };
