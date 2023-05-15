a58adfe54d6422c300997fd7176d952781d0650d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-15 23:10:33 +0100

    Time: 146 ms (52.83%), Space: 69.5 MB (49.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> u;
-        for (int n: nums){
-            if (u.count(n)>0) return true;
-            u.insert(n);
-        }return false;
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        int n = nums.size(), x = 0;
+        unordered_map<int,int> m;
+        
+        for (int i = 0; i<n; i++){
+            
+            x = nums[i];
+            if (m.find(x) != m.end() && i-m[x]<=k)
+                return true;
+            m[x] = i;
+        }
+        
+        return false;
     }
 };
