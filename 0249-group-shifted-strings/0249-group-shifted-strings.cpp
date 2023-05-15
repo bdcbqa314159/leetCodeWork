873000d79108db5b19fcd9eb10547f8ac7faba53
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-15 23:30:34 +0100

    Time: 4 ms (75.65%), Space: 8.1 MB (55.96%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.cpp
+++ 0249-group-shifted-strings/0249-group-shifted-strings.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    vector<vector<string>> groupStrings(vector<string>& strings) {
-        unordered_map<string, vector<string> > mp;
-        for (string  s : strings)
-            mp[shift(s)].push_back(s);
-        vector<vector<string> > groups;
-        for (auto m : mp) {
-            vector<string> group = m.second;
-            sort(group.begin(), group.end());
-            groups.push_back(group);
-        }
-        return groups;
-    }
-private:
-    string shift(string& s) {
+    
+    string shift(string &s){
+        
         string t;
-        int n = s.length();
-        for (int i = 1; i < n; i++) {
-            int diff = s[i] - s[i - 1];
-            if (diff < 0) diff += 26;
-            t += 'a' + diff + ',';
+        int n = size(s);
+        for (int i = 1; i<n; i++){
+            int diff = s[i]-s[i-1];
+            if (diff<0) diff+=26;
+            t+='a'+diff+',';
         }
+        
         return t;
+    }
+    
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        vector<vector<string>> result;
+        unordered_map<string,vector<string>> umap;
+        
+        for (auto s: strings)
+            umap[shift(s)].push_back(s);
+        
+        for (auto item:umap)
+            result.push_back(item.second);
+        return result;
     }
 };
