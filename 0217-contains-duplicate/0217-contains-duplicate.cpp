9a0617e52c2aada8c4ac7afb6e197114a96f0702
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-15 23:08:57 +0100

    Time: 139 ms (65.24%), Space: 69.5 MB (59.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> u;
-        for (int n : nums){
+        for (int n: nums){
             if (u.count(n)>0) return true;
             u.insert(n);
         }return false;
