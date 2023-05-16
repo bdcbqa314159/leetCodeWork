94ec8281567d18681392b915ce4b7c0674408b90
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 09:52:22 +0100

    Time: 245 ms (53.42%), Space: 96.9 MB (74.97%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -34,6 +34,42 @@ public:
         return v[rand()%v.size()];
     }
 };
+// class RandomizedSet {
+// public:
+    
+//     vector<int> v;
+//     unordered_map<int,int>mp;
+    
+//     RandomizedSet() {
+        
+//     }
+    
+//     bool search(int val){
+//         return mp.find(val)!=mp.end();
+//     }
+    
+//     bool insert(int val) {
+//         if (!search(val)) return false;
+//         v.push_back(val);
+//         mp[val] = v.size()-1;
+//         return true;
+//     }
+    
+//     bool remove(int val) {
+//         if (!search(val)) return false;
+        
+//         auto it = mp.find(val);
+//         v[it->second] = v.back();
+//         v.pop_back();
+//         mp[v[it->second]] = it->second;
+//         mp.erase(val);
+//         return true;
+//     }
+    
+//     int getRandom() {
+//         return v[rand()%v.size()];
+//     }
+// };
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
