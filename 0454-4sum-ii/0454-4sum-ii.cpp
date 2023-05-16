68330b4b979d47c8bf95699a3fc6ae4e183def61
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 10:11:38 +0100

    Time: 320 ms (33.82%), Space: 24.4 MB (77.11%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -5,18 +5,17 @@ public:
         unordered_map<int,int> umap;
         for (int x: nums1){
             for (int y: nums2){
-                if (umap.find(x+y) == umap.end())
-                    umap[x+y] = 1;
-                
+                if (umap.find(x+y)==umap.end())
+                    umap[x+y]=1;
                 else umap[x+y]++;
             }
         }
+        
         for (int x: nums3){
             for (int y: nums4){
                 int target = -(x+y);
                 if (umap.find(target) != umap.end())
                     ans+=umap[target];
-            
             }
         }
         return ans;
