3c5d88efef5cb8368097b71cdd795fe1307050c6
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 09:58:34 +0100

    Time: 23 ms (80.18%), Space: 10.6 MB (92.27%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> ans(26);
         for (char c: s)
             ans[c-'a']++;
-        for (int i = 0; i<n; i++){
-            if (ans[s[i]-'a']==1) return i;
+        for (int i =0; i<n; i++){
+            if (ans[s[i]-'a'] == 1) return i;
         }
         return -1;
     }
