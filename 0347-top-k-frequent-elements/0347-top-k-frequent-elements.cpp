e148f2caf46f757eaa75796272e76e5ef00775f2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 08:24:43 +0100

    Time: 24 ms (18.91%), Space: 13.6 MB (71.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,23 @@
 class Solution {
+public:
     
-    struct greater_than
-    {
-        inline bool operator() (const pair<int, int>& left, const pair<int, int>& right)
-        {
-            return (left.second > right.second);
+    struct comp{
+        inline bool operator()(const pair<int,int>&l, const pair<int,int>&r){
+            return l.second>r.second;
         }
     };
     
-public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> m;
+        vector<pair<int,int>> v;
         
-        map<int, int> mymap;
-        vector<pair<int, int> > myvec;
-        for(int ele : nums) mymap[ele] += 1;
-        for(auto ele : mymap) myvec.push_back(make_pair(ele.first, ele.second));
-        sort(myvec.begin(), myvec.end(), greater_than());
-        
-        vector<int> result;
-        for(int i = 0;i < k;i++)
-            result.push_back(myvec[i].first);
+        for (int x: nums) m[x]++;
+        for (auto x: m)
+            v.push_back(make_pair(x.first,x.second));
+        sort(v.begin(), v.end(), comp());
+        vector<int>result;
+        for (int i = 0; i<k; i++)
+            result.push_back(v[i].first);
         return result;
     }
 };
