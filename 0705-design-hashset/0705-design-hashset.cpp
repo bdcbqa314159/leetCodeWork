f7a81a6311be3c9cd194a8a00d69fc7e2fe6f421
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 11:49:56 +0100

    Time: 108 ms (52.09%), Space: 53.7 MB (29.98%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,42 @@
+class MyHashSet {
+public:
+    
+    int numBuckets = 15000;
+    vector<vector<int>> buckets = vector<vector<int>>(numBuckets, vector<int>{});
+    
+    int hash(int key){
+        return key%numBuckets;
+    }
+    
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        int i = hash(key);
+        auto it = find(buckets[i].begin(), buckets[i].end(), key);
+        if (it == buckets[i].end())
+            buckets[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        int i = hash(key);
+        auto it = find(buckets[i].begin(), buckets[i].end(), key);
+        if (it!=buckets[i].end())
+            buckets[i].erase(it);
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        auto it = find(buckets[i].begin(), buckets[i].end(), key);
+        return it!=buckets[i].end();
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
