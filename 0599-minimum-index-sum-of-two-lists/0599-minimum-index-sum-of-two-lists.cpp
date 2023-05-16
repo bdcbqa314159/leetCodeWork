adb03a8b786d5625fe40a6674152cf24dcab9db3
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 11:15:49 +0100

    Time: 98 ms (59.78%), Space: 36.6 MB (63.93%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        
         int n = list1.size(), p = list2.size();
         vector<string> result;
-        
         int min_index = INT_MAX;
-        map<string,int> m;
+        map<string, int> m;
         
         for (int i = 0; i<n; i++)
             m[list1[i]] = i;
@@ -15,15 +15,16 @@ public:
             if (it == m.end()) continue;
             
             int index = it->second+j;
-            
             if (index == min_index)
                 result.push_back(list2[j]);
             if (index<min_index){
-                min_index=index;
+                min_index = index;
                 result.clear();
                 result.push_back(list2[j]);
             }
         }
+        
+        
         return result;
     }
 };
