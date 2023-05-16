8f424ac67791eb236678ed18d656f2d87f142c2d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-05-16 09:12:16 +0100

    Time: 63 ms (96.40%), Space: 32.5 MB (59.64%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.cpp
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.cpp
@@ -1,14 +1,14 @@
 class Logger {
 public:
     
-    map<string, int>m;
+    map<string, int> m;
     
     Logger() {
         
     }
     
     bool shouldPrintMessage(int timestamp, string message) {
-        if (m.count(message)>0 && m[message]+10>timestamp)
+        if(m.count(message)>0 && m[message]+10>timestamp)
             return false;
         m[message] = timestamp;
         return true;
