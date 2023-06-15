b2e97b5095ab5170ba7b5a880f51fb2413c8bdb4
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-06-15 20:38:18 +0100

    Time: 77 ms (58.82%), Space: 16.5 MB (10.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,14 +3,13 @@ class Solution:
         n = len(s)
         ans = 0
         if n == 0: return ans
-        l = r = 0
         umap = {}
-        while (l<n and r<n):
+        l=r=0
+        while l<n and r<n:
             e = s[r]
             if e in umap:
                 l = max(l, umap[e]+1)
             ans = max(ans, r-l+1)
-            umap[e] = r
+            umap[e]=r
             r+=1
         return ans
-        
