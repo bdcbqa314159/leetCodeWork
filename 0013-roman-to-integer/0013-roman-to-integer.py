01e3700bae2d63c6c138c206a5ea078a7ff997ef
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-06-15 23:44:00 +0100

    Time: 65 ms (45.87%), Space: 16.4 MB (8.50%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    def romanToInt(self, s: str) -> int:
+        prev = cur = total = 0
+        for i in range(len(s)):
+            cur = Solution.dic[s[i]]
+            
+            if cur>prev:
+                total += cur-2*prev
+            else:
+                total+=cur
+            
+            prev = cur
+            
+        return total
+        
