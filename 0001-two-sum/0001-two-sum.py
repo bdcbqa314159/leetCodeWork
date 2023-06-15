87837d9c2374866f47fc9f541ce100329b304c6c
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-06-15 20:31:47 +0100

    Time: 65 ms (90.90%), Space: 17.7 MB (13.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,9 +3,8 @@ class Solution:
         n = len(nums)
         umap = {}
         for i in range(n):
-            diff = target - nums[i]
+            diff = target-nums[i]
             if diff not in umap:
                 umap[nums[i]] = i
             else: return [i, umap[diff]]
         return []
-        
