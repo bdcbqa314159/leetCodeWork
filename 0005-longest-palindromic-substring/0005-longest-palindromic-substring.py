c3142afc828ce44655b8424444689b9b62aeeb75
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-06-15 23:25:13 +0100

    Time: 4771 ms (21.30%), Space: 24.4 MB (8.09%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,16 +3,15 @@ class Solution:
         n = len(s)
         if n<2:
             return s
+        
         l,r = 0,0
         dp = [[0]*n for i in range(n)]
         
         for j in range(1,n):
             for i in range(0,j):
-                current_Palindrome = dp[i+1][j-1] or j-i<=2
-                if s[i] == s[j] and current_Palindrome:
+                curr_palindrome = dp[i+1][j-1] or j-i<=2
+                if s[i] == s[j] and curr_palindrome:
                     dp[i][j] = True
-                    if j-i > r-l:
-                        l = i
-                        r = j
+                    if j-i>r-l:
+                        l, r = i, j
         return s[l:r+1]
-        
