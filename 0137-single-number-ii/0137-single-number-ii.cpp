ac6f4003fc65cf46d6f5b0416a38abea90a7fcf2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-07-04 22:38:18 +0100

    Time: 8 ms (67.34%), Space: 9.5 MB (72.42%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0, threes = 0;
+        
+        for (int n: nums){
+            twos |= ones&n;
+            ones ^=n;
+            threes = ones&twos;
+            
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        
+        return ones;
+    }
+};
