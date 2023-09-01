81c1f096547bce2f7349d0780df9db630a8ac3f1
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-02 00:35:22 +0100

    Time: 0 ms (100.00%), Space: 7.9 MB (65.79%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> bits(n+1);
+        bits[0] = 0;
+        for (int i = 1; i<=n; i++)
+            bits[i]=bits[i/2]+i%2;
+        return bits;
+    }
+};
+
