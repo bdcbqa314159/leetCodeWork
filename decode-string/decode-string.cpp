9664e17048d29306b495142386ea2a314beda236
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-10 22:54:00 +0100

    Time: 0 ms, Memory: 7.6 MB - LeetHub

---
+++ decode-string/decode-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> stringStack;
+        string current;
+        int k = 0;
+        for (auto ch: s){
+            if (isdigit(ch))
+                k = k*10 +ch-'0';
+            else if(ch == '['){
+                countStack.push(k);
+                stringStack.push(current);
+                current = "";
+                k = 0;
+            }
+            else if(ch == ']'){
+                string decodedString = stringStack.top();
+                stringStack.pop();
+                for (int count = countStack.top(); count>0; count--){
+                    decodedString = decodedString+current;
+                }
+                countStack.pop();
+                current = decodedString;
+            }
+            else{
+                current += ch;
+            }
+            
+        }
+        return current;
+    }
+};
