2a1f56ecb4f70d62e2f760512a81dc8c17964db2
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-10 17:58:56 +0100

    Time: 0 ms, Memory: 6.4 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    bool isValid(char a, char b){
+        return (a=='(' && b==')' || a=='[' && b==']' || a=='{' && b=='}');
+    }
+    
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0;i<s.size(); i++){
+            if (st.size() > 0){
+                char li = st.top();
+                if (isValid(li, s[i])){
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(s[i]);
+        }
+        return st.size() == 0;
+    }
+};
