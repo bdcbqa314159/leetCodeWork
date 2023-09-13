6e578541223217bd7902538be0d2059b5bbc2135
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-13 21:45:46 +0100

    Time: 45 ms (23.38%), Space: 16.5 MB (36.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -5,9 +5,9 @@ public:
         int c = grid[0].size();
         
         int ans = 0;
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
+        vector<vector<bool>> visited(r, vector<bool>(c,false));
         queue<pair<int,int>> q;
-        vector<vector<int>>dir = {{0,1}, {0,-1}, {-1,0},{1,0}};
+        vector<vector<int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
         
         for (int i = 0; i<r; i++){
             for (int j = 0; j<c; j++){
@@ -24,12 +24,13 @@ public:
                         if (x<0||x>=r||y<0||y>=c||visited[x][y]||grid[x][y]=='0')
                             continue;
                         visited[x][y] = true;
-                        for (int d = 0; d<dir.size(); d++)
+                        for (int d = 0; d<dir.size();d++)
                             q.push(make_pair(x+dir[d][0], y+dir[d][1]));
                     }
                 }
             }
         }
         return ans;
+    
     }
 };
