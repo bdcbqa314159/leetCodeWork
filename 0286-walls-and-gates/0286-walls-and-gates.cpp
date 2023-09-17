aed577cd981d0485d1eb63cc84fac89db92fb711
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-17 15:27:33 +0100

    Time: 20 ms (94.01%), Space: 14.7 MB (51.99%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        int n = rooms.size();
-        int m = rooms[0].size();
+        int n = rooms.size(), m = rooms[0].size();
         queue<pair<int,int>> q;
-        for (int i = 0; i<rooms.size(); i++){
-            for (int j = 0; j<rooms[0].size(); j++){
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++){
                 if (rooms[i][j] == 0)
                     q.push({i,j});
             }
@@ -14,29 +13,26 @@ public:
         while (!q.empty()){
             int i = q.front().first, j = q.front().second;
             q.pop();
-            int dist = rooms[i][j] + 1;
+            int dist = rooms[i][j]+1;
             
-            if (i+1<n && rooms[i+1][j]>dist){
+            if (i+1 < n && rooms[i+1][j]>dist){
                 rooms[i+1][j] = dist;
                 q.push({i+1,j});
             }
-            
-            if (i-1>=0 && rooms[i-1][j]>dist){
+            if (i-1 >= 0 && rooms[i-1][j]>dist){
                 rooms[i-1][j] = dist;
                 q.push({i-1,j});
             }
-            
-            if (j+1<m && rooms[i][j+1]>dist){
+            if (j+1 < m && rooms[i][j+1]>dist){
                 rooms[i][j+1] = dist;
                 q.push({i,j+1});
             }
-            
-            if (j-1>=0 && rooms[i][j-1]>dist){
+            if (j-1 >= 0 && rooms[i][j-1]>dist){
                 rooms[i][j-1] = dist;
                 q.push({i,j-1});
             }
-            
         }
+        
         return;
     }
 };
