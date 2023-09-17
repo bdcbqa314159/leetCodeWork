2af130f4f19bf2694f5de2b7d5bf7c9ef006e1b7
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-17 15:46:17 +0100

    Time: 0 ms (100.00%), Space: 8.2 MB (16.40%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,35 +1,35 @@
-
 class Solution {
 public:
     string decodeString(string s) {
         stack<int> countStack;
         stack<string> stringStack;
-        string currentString;
+        string current;
+        
         int k = 0;
-        for (auto ch : s) {
-            if (isdigit(ch)) {
-                k = k * 10 + ch - '0';
-            } else if (ch == '[') {
-                // push the number k to countStack
+        for (auto ch : s){
+            if (isdigit(ch))
+                k = k*10+ch-'0';
+            else if (ch == '['){
                 countStack.push(k);
-                // push the currentString to stringStack
-                stringStack.push(currentString);
-                // reset currentString and k
-                currentString = "";
+                stringStack.push(current);
+                current = "";
                 k = 0;
-            } else if (ch == ']') {
-                string decodedString = stringStack.top();
+            }
+            else if (ch == ']'){
+                string decodedString  = stringStack.top();
                 stringStack.pop();
-                // decode currentK[currentString] by appending currentString k times
-                for (int currentK = countStack.top(); currentK > 0; currentK--) {
-                    decodedString = decodedString + currentString;
+                for (int count = countStack.top(); count>0; count--){
+                    decodedString = decodedString+current;
                 }
                 countStack.pop();
-                currentString = decodedString;
-            } else {
-                currentString = currentString + ch;
+                current = decodedString;
+                
             }
+            else{
+                current+=ch;
+            }
+            
         }
-        return currentString;
+        return current;
     }
 };
