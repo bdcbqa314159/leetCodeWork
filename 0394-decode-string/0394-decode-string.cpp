0b566f9fb407a63666676bd580f764ec29b3377d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-17 16:00:47 +0100

    Time: 3 ms (39.38%), Space: 7 MB (24.46%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -15,19 +15,16 @@ public:
                 current = "";
                 k = 0;
             }
-            else if (ch == ']'){
+            else if(ch == ']'){
                 string decoded = strings.top();
                 strings.pop();
                 for (int count = counter.top(); count>0; count--)
-                    decoded += current;
+                    decoded+=current;
                 counter.pop();
-                current = decoded;
+                current =decoded;
             }
-            else
-                current += ch;
+            else current+=ch;
         }
-        
-        
         return current;
     }
 };
