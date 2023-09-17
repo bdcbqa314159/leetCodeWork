624a2b41cd2a620a061c0f8989183896d5a78489
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-17 17:05:00 +0100

    Time: 268 ms (40.03%), Space: 62.7 MB (7.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     
-    int dp(vector<int>&nums, int target, int idx, map<pair<int,int>,int>& memo){
+    int dp(vector<int>& nums, int target, int idx, map<pair<int, int>, int>& memo){
         if (idx == nums.size()) return target == 0;
-        if (memo.count({idx, target})) return memo[{idx, target}];
-        
-        int pos = dp(nums, target+nums[idx], idx+1,memo);
+        if (memo.count({idx, target})) return memo[{idx,target}];
+        int pos = dp(nums, target+nums[idx], idx+1, memo);
         int neg = dp(nums, target-nums[idx], idx+1, memo);
         return memo[{idx, target}] = pos+neg;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        map<pair<int, int>, int> memo;
+        map<pair<int,int>, int> memo;
         return dp(nums, target, 0, memo);
+        
     }
 };
