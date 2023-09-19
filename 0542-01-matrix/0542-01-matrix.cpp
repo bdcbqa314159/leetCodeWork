c3cb4f3e515402bb37737f6fde0655aec4bb5acd
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-19 19:39:28 +0100

    Time: 61 ms (66.58%), Space: 30.8 MB (49.91%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -21,9 +21,9 @@ public:
         
         const int dx[4] = {-1,0,1,0};
         const int dy[4] = {0, 1, 0, -1};
-        // while (!q.empty()){
-        //     pair<int,int> node = q.front();
-        // q.pop();
+        while (!q.empty()){
+            pair<int,int> node = q.front();
+        q.pop();
         // for (int t = 0; t<4; t++){
         //     pair<int,int> neighbor = {node.first+dx[t],node.second+dy[t]};
         //     if (isValid(neighbor,n,m) && dist[neighbor.first][neighbor.second] == -1){
@@ -31,11 +31,6 @@ public:
         //         q.push(neighbor);
         //     }
         // }
-        // }
-        
-        while(!q.empty()){
-            pair<int,int> node = q.front();
-            q.pop();
             for (int t = 0; t<4; t++){
                 pair<int,int> neighbor = {node.first+dx[t], node.second+dy[t]};
                 if (isValid(neighbor, n, m) && dist[neighbor.first][neighbor.second] == -1){
@@ -44,6 +39,18 @@ public:
                 }
             }
         }
+        
+        // while(!q.empty()){
+        //     pair<int,int> node = q.front();
+        //     q.pop();
+        //     for (int t = 0; t<4; t++){
+        //         pair<int,int> neighbor = {node.first+dx[t], node.second+dy[t]};
+        //         if (isValid(neighbor, n, m) && dist[neighbor.first][neighbor.second] == -1){
+        //             dist[neighbor.first][neighbor.second] = dist[node.first][node.second]+1;
+        //             q.push(neighbor);
+        //         }
+        //     }
+        // }
         return dist;
         
     }
