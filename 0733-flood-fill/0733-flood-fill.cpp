debc727766107971030eaeaf29fc91e03479fe72
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-19 20:13:57 +0100

    Time: 8 ms (55.85%), Space: 14.2 MB (42.61%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+//     void fill(vector<vector<int>>&image, int sr, int sc, int color, int newColor){
+//         int n = image.size(), m = image[0].size();
+//         if (sr<0||sc<0||sr>=n||sc>=m||image[sr][sc]!=color) return;
+//         image[sc][sr] = newColor;
+//         fill(image, sr-1,sc, color, newColor);
+//         fill(image, sr+1,sc, color, newColor);
+//         fill(image, sr,sc-1, color, newColor);
+//         fill(image, sr,sc+1, color, newColor);
+        
+//     }
+    void fill(vector<vector<int>>& image, int sr, int sc, int color, int newColor){
+        if (sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size()||image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        
+        fill(image, sr-1, sc, color, newColor);
+        fill(image, sr+1, sc, color, newColor);
+        fill(image, sr, sc-1, color, newColor);
+        fill(image, sr, sc+1, color, newColor);
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
+        fill(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+};
