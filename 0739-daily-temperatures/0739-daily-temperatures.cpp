e4eb4983ba0f9b1ff83ffa171cb2ee0d54dba437
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-19 22:57:47 +0100

    Time: 153 ms (32.73%), Space: 98.8 MB (66.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int,int>> st;
         vector<int> result(temperatures.size());
+        stack<pair<int,int>> st;
         
         for (int i = temperatures.size()-1; i>=0; i--){
             int curr = temperatures[i];
             int day = -1;
             while (!st.empty()){
-                if (curr >= st.top().second)
+                if (curr>= st.top().second)
                     st.pop();
                 else{
                     day = st.top().first;
@@ -18,7 +18,7 @@ public:
             }
             if (st.empty())
                 st.push({i,curr});
-            result[i] = day == -1 ? 0:day-i;
+            result[i] = day == -1?0:day-i;
         }
         return result;
     }
