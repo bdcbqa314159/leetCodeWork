3487c646785073e1c2d231e53b78dd1b69a39efd
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-09-19 19:52:40 +0100

    Time: 60 ms (69.97%), Space: 30.7 MB (50.09%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     bool isValid(pair<int,int>& cell, int n, int m){
         if (cell.first<0||cell.first>=n||cell.second<0||cell.second>=m) return false;
         return true;
@@ -10,7 +9,7 @@ public:
         int n = mat.size(), m = mat[0].size();
         vector<vector<int>> dist(n, vector<int>(m,-1));
         queue<pair<int,int>> q;
-        for (int i = 0; i<n; i++){
+        for (int i =0; i<n; i++){
             for (int j = 0; j<m; j++){
                 if (mat[i][j] == 0){
                     dist[i][j] = 0;
@@ -20,39 +19,23 @@ public:
         }
         
         const int dx[4] = {-1,0,1,0};
-        const int dy[4] = {0, 1, 0, -1};
+        const int dy[4] = {0,-1,0,1};
+        
         while (!q.empty()){
-            pair<int,int> node = q.front();
-        q.pop();
-        // for (int t = 0; t<4; t++){
-        //     pair<int,int> neighbor = {node.first+dx[t],node.second+dy[t]};
-            // if (isValid(neighbor,n,m) && dist[neighbor.first][neighbor.second] == -1){
-                // dist[neighbor.first][neighbor.second]=dist[neighbor.first][neighbor.second]+1;
-        //         q.push(neighbor);
-        //     }
-        // }
+            pair<int, int> node = q.front();
+            q.pop();
+            int i = node.first, j = node.second;
             for (int t = 0; t<4; t++){
-                pair<int,int> neighbor = {node.first+dx[t], node.second+dy[t]};
-                if (isValid(neighbor,n,m) && dist[neighbor.first][neighbor.second] == -1){
-            dist[neighbor.first][neighbor.second]=dist[node.first][node.second]+1;
-                    q.push(neighbor);
+                pair<int, int> neighbor = {i+dx[t], j+dy[t]};
+                int ii = neighbor.first, jj = neighbor.second;
+                if (isValid(neighbor, n,m) && dist[ii][jj] == -1){
+                    dist[ii][jj] = dist[i][j]+1;
+                q.push(neighbor);
+                    
                 }
+                    
             }
         }
-        
-        // while(!q.empty()){
-        //     pair<int,int> node = q.front();
-        //     q.pop();
-        //     for (int t = 0; t<4; t++){
-        //         pair<int,int> neighbor = {node.first+dx[t], node.second+dy[t]};
-        //         if (isValid(neighbor, n, m) && dist[neighbor.first][neighbor.second] == -1){
-        //             dist[neighbor.first][neighbor.second] = dist[node.first][node.second]+1;
-        //             q.push(neighbor);
-        //         }
-        //     }
-        // }
         return dist;
-        
     }
-    
 };
