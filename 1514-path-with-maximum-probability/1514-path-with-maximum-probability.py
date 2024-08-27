77a9817f9b4fbf7703940541294f57b487c1d717
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-08-27 23:38:07 +0100

    Time: 590 ms (20.27%), Space: 27.9 MB (97.67%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,23 +1,22 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        max_prob = [0] * n
-        max_prob[start] = 1
+        graph = defaultdict(list)
+        for i, (a, b) in enumerate(edges):
+            graph[a].append([b, succProb[i]])
+            graph[b].append([a, succProb[i]])
+            
+        max_prob = [0.0] * n    
+        max_prob[start] = 1.0
         
-        for i in range(n - 1):
-            # If there is no larger probability found during an entire round of updates,
-            # stop the update process.
-            has_update = 0
-            for j in range(len(edges)):
-                u, v = edges[j]
-                path_prob = succProb[j]
-                if max_prob[u] * path_prob > max_prob[v]:
-                    max_prob[v] = max_prob[u] * path_prob
-                    has_update = 1 
-                if max_prob[v] * path_prob > max_prob[u]:
-                    max_prob[u] = max_prob[v] * path_prob
-                    has_update = 1
-            if not has_update:
-                break
-        
+        queue = deque([start])
+        while queue:
+            cur_node = queue.popleft()
+            for nxt_node, path_prob in graph[cur_node]:
+
+                # Only update max_prob[nxt_node] if the current path increases
+                # the probability of reach nxt_node.
+                if max_prob[cur_node] * path_prob > max_prob[nxt_node]:
+                    max_prob[nxt_node] = max_prob[cur_node] * path_prob
+                    queue.append(nxt_node)
+                    
         return max_prob[end]
-        
